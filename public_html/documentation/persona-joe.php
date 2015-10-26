5c756af5745dacae82040bb3a26b2b6440f06a0b
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-26 15:42:31 -0600

    Added more details to the small food bank persona

--- public_html/documentation/persona-joe.php
+++ public_html/documentation/persona-joe.php
@@ -7,7 +7,9 @@
 	<body>
 		<h1>Persona: Joe (Small Food Bank)</h1>
 		<p>The name of this persona is Joe.  Joe is a man in his 40's, who runs a small food bank with a small team, and often does most of the grunt work himself.</p>
-		<p>Joe's food bank doesn't have a large amount of room (particularly for refrigerated food), so they have to be pretty selective about what they take.  He also has to pick up the deliveries in a fairly small truck with no refrigeration, and he often pays for the gas out-of-pocket; trips that end with no actual food being picked up are a significant inconvenience. Joe works with the foodbank entirely out of love for the community; he is not very wealthy, and the food bank he operates does nothave access to a very large pool of resources.</p>
+		<p>Joe's food bank doesn't have a large amount of room (particularly for refrigerated food), so they have to be pretty selective about what they take.  He also has to pick up the deliveries in a fairly small truck with no refrigeration, and he often pays for the gas out-of-pocket; trips that end with no actual food being picked up are a significant inconvenience. </p>
+		<p>Joe works with the foodbank entirely out of love for the community; he is not very wealthy, and the food bank he operates does nothave access to a very large pool of resources.  He cares enough that he's willing to personally go above and beyond to keep his food bank stocked, but he may not necessarily have the means to do so</p>
+		<p>This site is a matter of convinence for Joe: it allows him to know when there is a new delivery to pick-up, and filter out those his food bank can't physically support.  He also needs to be sure that the any delivery he travels to get is there for him, as wasted trips do have a significant impact.</p>
 		<p>Joe is not a very technically-minded person, and he does not have access to the latest in technology.  He is most likely going to be using this site on a food bank desktop running an older version of Windows (Windows 7 at the newest), and will likely never be viewing the site on a smartphone.</p>
 	</body>
 </html>
