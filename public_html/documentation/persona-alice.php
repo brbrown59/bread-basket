865ab96431802320a15ee01d52c2009bad9fb02b
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-26 17:03:51 -0600

    added setup for Alice, the coop persona

---
+++ public_html/documentation/persona-alice.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Persona for Alice Who Works at The Coop</title>
+	</head>
+	<body>
+		<h1>Alicea</h1>
+	</body>
+
+</html>
