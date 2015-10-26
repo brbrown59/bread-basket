442da76c6827b01ec2d22ff2a2aba51a1f8cfb59
Carlos Beraun <cberaun2@cnm.edu>
2015-10-26 17:04:50 -0600

    Added persona-bill to documentation

--- depolymentbbt.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-	<html>
-	<meta charset="uft-8">
-
-	<body>
-<p>sample</p>
-	</body>
-</html>
---
+++ public_html/documentation/person-bill.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset = "utf-8">
+		<title>Persona: Joe</title>
+	</head>
+	<body>
+		<h1>Persona: Bill(large Food Bank)</h1>
+
+	</body>
+</html>
