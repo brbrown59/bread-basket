994fa1ada2ec8c1db6c1bfbe999cc2b71300f081
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-10-27 05:30:34 -0600

    Fixed typos, so I could push.

--- public_html/documentation/persona-julia-rrfb.php
+++ public_html/documentation/persona-julia-rrfb.php
@@ -5,8 +5,8 @@
 		<title>Persona: Joe</title>
 	</head>
 	<body>
-		<h1>Persona: Julie (Large Food Bank)</h1>
-		<p>The name of this persona is Julie.  Julie is 38 years old, and works full-time as the manager of Food Industry Donors at Road Runner Food Bank. As part of her job she works with a number of grocery stores in the Grocery Rescue program. She has a fleet of refrigerated truck out in the community every day transporting food from the places that have it to the food banks and the people who  need it. Grocery stores donations are an incredibly important part of meeting her goal to feeding hungry New Mexicans. Julie is not especially tech savvy, but she does have a an older smartphone. </p>
+		<h1>Persona: Julia (Large Food Bank)</h1>
+		<p>The name of this persona is Julia.  Julia is 38 years old, and works full-time as the manager of Food Industry Donors at Road Runner Food Bank. As part of her job she works with a number of grocery stores in the Grocery Rescue program. She has a fleet of refrigerated truck out in the community every day transporting food from the places that have it to the food banks and the people who  need it. Grocery stores donations are an incredibly important part of meeting her goal to feeding hungry New Mexicans. Julia is not especially tech savvy, but she does have a an older smartphone. </p>
 		<p>As a food bank representative, she has signed up with the Bread Basket web app, so she gets an email when she’s at her desk and/or a push notification on her phone when she’s out in the warehouse. These notification tell her about food donations that become available throughout the day. After she sees what’s being donated, she decides whether or not to accept the donation through the web app and radios a truck to go by and pick the donation up. </p>
 	</body>
 </html>
