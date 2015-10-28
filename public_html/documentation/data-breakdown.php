a3512b3c7be60a80007735b4775e033ba6964aec
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-28 16:06:43 -0600

    started data breakdown

---
+++ public_html/documentation/data-breakdown.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Breakdown</title>
+	</head>
+	<body>
+		<h1>Data Breakdown</h1>
+		<p>Organization</p>
+		<ul>
+			<li>orgID</li>
+			<li>orgName</li>
+			<li>orgType</li>
+			<li>orgDescription</li>
+			<li>orgLocation</li>
+			<li>orgHours</li>
+			<li>orgEmail</li>
+		</ul>
+		<p>Admin</p>
+		<p>Volunteer</p>
+		<p>Connection</p>
+		<p>Message/Notification</p>
+		<p>Listing</p>
+		<p></p>
+	</body>
+</html>
