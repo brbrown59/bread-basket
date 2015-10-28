1cf3f71b6ec5010349162125eb59d540b398cb09
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-28 15:54:15 -0600

    Added assignments for Use Cases

--- public_html/documentation/use-cases.php
+++ public_html/documentation/use-cases.php
@@ -6,14 +6,14 @@
 	</head>
 	<body>
 		<h2>User Stories</h2>
-		<p>As a public user, I want to register for Bread Basket and create a profile.</p>
-		<p>As a registered user, I want to put out a message that my grocery store has food available.</p>
-		<p>As a registered user, I want to delete my earlier food listing.</p>
-		<p>As a registered user, I want to respond to a reminder notification regarding my food listing.</p>
-		<p>As an administrator, I want to claim an available set of food for my food bank.</p>
-		<p>As an administrator, I want to push a notification about claimed food to my volunteers.</p>
-		<p>As an administrator, I want to add a list of volunteer contacts to my food bank.</p>
-		<p>As a registered user, I want to respond to a notification from a food bank and agree to pick up a claimed set of food</p>
+		<p>As a public user, I want to register for Bread Basket and create a profile.Bradley</p>
+		<p>As a registered user, I want to put out a message that my grocery store has food available.Bradley</p>
+		<p>As a registered user, I want to delete my earlier food listing.Kimberly</p>
+		<p>As a registered user, I want to respond to a reminder notification regarding my food listing.Kimberly</p>
+		<p>As an administrator, I want to claim an available set of food for my food bank.Tamra</p>
+		<p>As an administrator, I want to push a notification about claimed food to my volunteers.Tamra</p>
+		<p>As an administrator, I want to add a list of volunteer contacts to my food bank.Carlos</p>
+		<p>As a registered user, I want to respond to a notification from a food bank and agree to pick up a claimed set of food.Carlos</p>
 		<h2>Use Cases</h2>
 	</body>
 </html>
