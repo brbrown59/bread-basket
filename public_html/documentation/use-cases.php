7fca3bc15eb999bcafd0d8a760bb0a9107cbad32
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-28 14:26:12 -0600

    Added some user stories

--- public_html/documentation/use-cases.php
+++ public_html/documentation/use-cases.php
@@ -5,6 +5,11 @@
 		<title>User Stories and Use Cases</title>
 	</head>
 	<body>
-		<p>Stuff goes here</p>
+		<h2>User Stories</h2>
+		<p>As a public user, I want to register for Bread Basket.</p>
+		<p>As a registered user, I want to put out a message that I have food available.</p>
+		<p>As a registered user, I want to claim an available set of food.</p>
+		<p>As a registered user, I want to notify the system that food I made available has been picked up.</p>
+		<h2>Use Cases</h2>
 	</body>
 </html>
