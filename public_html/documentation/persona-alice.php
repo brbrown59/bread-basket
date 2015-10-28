ce38c69c0f09c2e2b67a7f572e3fa8cde6ead1db
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-28 13:28:02 -0600

    added persona for Alice, the coop persona

--- public_html/documentation/persona-alice.php
+++ public_html/documentation/persona-alice.php
@@ -5,7 +5,13 @@
 		<title>Persona for Alice Who Works at The Coop</title>
 	</head>
 	<body>
-		<h1>Alice</h1>
+		<h1>Alice The Small, Local, Health Food Grocery Store Manager</h1>
+		<p>Alice is the team leader and manager at her branch of the local health store. She is environmentally and socially conscious.
+		Her small store composts and donates any unsellable food. She is personally invested in the reduction of food waste and the distribution
+		of usable goods to people in need. She owns a smart phone and knows her way around the basic functions of a computer, but needs quick, intutive
+		access to apps or software that she uses. Her frustrations include managing her store and employees while putting her best effort into following the
+		ethical codes and considerations of her store. She is not the owner of the store, but is invested in it's mission.
+		</p>
 	</body>
 
 </html>
