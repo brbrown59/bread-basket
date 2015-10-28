84a93512d905dcf49b78a4b74abc8792e835a605
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-28 13:50:33 -0600

    finished persona for keith, the small bakery owner

--- public_html/documentation/persona-keith.php
+++ public_html/documentation/persona-keith.php
@@ -6,7 +6,11 @@
 	</head>
 	<body>
 		<h1>Keith Who Owns a Small Bakery/Restaurant</h1>
-		<p>
+		<p>Keith is the co-owner of a small restaurtant business in his early 30s. They are primarily a bakery but also serve other food.
+			They're margins are tight. Keith wants to donate food for a good cause, but he can't take time off his shift to make sure food gets to
+			a donation center and all his employees already work hard at the bakery. He needs a fast way to communicate with the donation centers in his area
+			so that he can tell them when he has food available and have the send a volunteer. Keith has a smart phone and a laptop or desktop at home. He's needs an
+			intuitive interface that he can use quickly at work between managing his employees and the operations of his restaurant.
 		</p>
 	</body>
 
