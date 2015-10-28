88663adf2933238c081d9c3697ced3fac680414d
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-28 13:29:00 -0600

    Filled out the skeleton of the epic, and created the missing files

---
+++ public_html/documentation/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>The Epic</title>
+	</head>
+	<body>
+		<h2>Executive Summary</h2>
+		<p>Stuff</p>
+		<h2>Personas</h2>
+		<a href="persona-alice.php" target="_self">Alice: manager at a grocery co-op</a><br/>
+		<a href="persona-joe.php" target="_self">Joe: small food bank owner</a><br/>
+		<a href="persona-julia-rrfb.php" target="_self">Julia: manager at Roadrunner Food Bank</a><br/>
+		<h2>User Stories and Use Cases</h2>
+		<a href="use-cases.php" target="_self">Use Cases</a>
+		<h2>Entity Relationship Diagram and Database Schema</h2>
+		<a href="use-cases.php" target="_self">ERD</a>
+	</body>
+</html>
---
+++ public_html/documentation/use-cases.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>User Stories and Use Cases</title>
+	</head>
+	<body>
+		<p>Stuff goes here</p>
+	</body>
+</html>
