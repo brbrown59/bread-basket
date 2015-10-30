e0050601ff33af0e1e919e75c84f74ed5bafad44
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-30 10:42:10 -0600

    Fixed some use cases

--- public_html/documentation/use-cases.php
+++ public_html/documentation/use-cases.php
@@ -39,7 +39,7 @@
 			<li>The site takes the information and either confirms the new listing has been created, or displays an error message if it was not.</li>
 		</ul>
 
-		<h3>Administrator-foodbank, I want to add a list of volunteer contacts to my food bank.Carlos</h3>
+		<h3>Use Case: Julia wishes to add a list of volunteer contacts to her food bank.</h3>
 		<ul>
 			<li>Administrator opens app</li>
 			<li>logs into app  by typing in password in the text field.</li>
@@ -54,28 +54,23 @@
 			<li>The App returns to list of volunteers, the newly added volunteer now appears on the list of volunteers.</li>
 		</ul>
 
-		<h3>As a registered user-julia, I want to respond to a notification from a food bank and agree to pick up a claimed set of food.Carlos</h3>
+		<h3>Use Case: Julia is an administrator that wants to claim an available set of food for her food bank, and then she wants to push a notification about claimed food to her volunteers.</h3>
 		<ul>
+			<li>Julia receives a push notification from the Bread Basket app.</li>
 			<li>Registered user/volunteer receives a push notice their phone.</li>
-			<li>Volunteer opens app</li>
-			<li>app opens and a alerted to a pending request flashes on phone.</li>
-			<li>user clicks alert and new page opens with additional info regarding clicked alert.</li>
-			<li>user reads alert and then clicks “commit button? and confirms to pick-up and deliver food items.</li>
+			<li>She takes a look at the message that includes the location and what is being donated.</li>
+			<li>Julia decides her food pantry can use the food and clicks the claim button.</li>
+			<li>She is taken to the page that announces that she has claimed the food.</li>
+			<li>She is then given the option to push a notice to her volunteers, and pushes the button to past the notification on to her volunteers</li>
+			<li>Within 10 minutes Julia receives a notification that Joe will be picking up the donation,.</li>
 		</ul>
 
-		<h3>As a registered user, I want to respond to a reminder notification regarding my food listing.</h3>
+		<h3>Use Case: Joe receives a notification from his food bank about available food, and wants to go pick it up</h3>
 		<ul>
-
-			<p>Julia, is an administrator that wants to claim an available set of food for her food bank, and then she wants to push a notification about claimed food to her volunteers.</p>
-			<ul>
-				<li>Julia receives a push notification from the Bread Basket app.</li>
-				<li>Registered user/volunteer receives a push notice their phone.</li>
-				<li>She takes a look at the message that includes the location and what is being donated.</li>
-				<li>Julia decides her food pantry can use the food and clicks the claim button.</li>
-				<li>She is taken to the page that announces that she has claimed the food.</li>
-				<li>She is then given the option to push a notice to her volunteers, and pushes the button to past the notification on to her volunteers</li>
-				<li>Within 10 minutes Julia receives a notification that Joe will be picking up the donation,.</li>
-			</ul>
+			<li>Joe gets a push notification from the site about the available food on his phone</li>
+			<li>The notification contains the information about the food, a prompt to claim the food, and a link to the listing on the site</li>
+			<li>Joe decides he can get the food, and clicks the "claim" option</li>
+			<li>The site processes his claim</li>
 		</ul>
 	</body>
 </html>
