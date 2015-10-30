bc31b0f48102a0fdee0f38b9554af0e15d120927
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-30 10:00:58 -0600

    Typo fixes

--- public_html/documentation/persona-julia-volunteer.php
+++ public_html/documentation/persona-julia-volunteer.php
@@ -7,10 +7,10 @@
 	<body>
 		<h1>Persona: Julia (Food Bank Supervisor)</h1>
 		<p>Julia is 38 years old. She is a part-time food bank manager at her church. She has a small crew of volunteers
-			that help the food bank on a regular basis. The food pantery is open on Wednesday and Saturday mornings for
+			that help the food bank on a regular basis. The food pantry is open on Wednesday and Saturday mornings for
 			food pick up. Julia's pantry serves families in need in the church's congregation and in the surrounding community.
-			Julia is not especially tech savvy but uses the bread basket app on her Galaxy 3 smart phone to claim food on
-			Tuesday's and Friday's to get the freshest foods for the pantry's open hours the next day.
+			Julia is not especially tech savvy, but uses the bread basket app on her Galaxy 3 smart phone to claim food on
+			Tuesdays and Fridays to get the freshest foods for the pantry's open hours the next day.
 		</p>
 	</body>
 </html>
