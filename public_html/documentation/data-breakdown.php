619cd389988efd9638183c29d60a31d8ae705d38
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-01 13:41:49 -0700

    adding details to data breakdown

--- public_html/documentation/data-breakdown.php
+++ public_html/documentation/data-breakdown.php
@@ -40,7 +40,7 @@
 			<li>volPhone</li>
 			<li>volActive</li>
 			<li>volPushNotif</li>
-			<li>orgId</li>
+			<li>orgId (foreign key)</li>
 			<li>Do we need an opt in for push notifications?</li>
 			<li>Do we need an active field, so we can make volunteer inactive instead of deleting?</li>
 			<li>What about a mute notification field? Would that be out of scope or easy to implement?</li>
@@ -51,6 +51,7 @@
 			<li>listingType</li>
 			<li>listingMemo</li>
 			<li>listingPickupTime</li>
+			<li>listingDateTime</li>
 			<li>orgName</li>
 			<li>orgEmail</li>
 			<li>orgPhone</li>
@@ -63,13 +64,13 @@
 			<li>orgZip</li>
 			<li>orgHours</li>
 			<li>messageId (primary key)</li>
-			<li>listingId</li>
-			<li>orgId</li>
+			<li>listingId (foreign key)</li>
+			<li>orgId (foreign key)</li>
 		</ul>
 		<p>Listing (robust) (recursive 1 to n)</p>
 		<ul>
 			<li>listingId (primary key)</li>
-			<li>orgId</li>
+			<li>orgId (foreign key)</li>
 			<li>listingType (what are the options here?)</li>
 			<li>listingCost</li>
 			<li>listingMemo (what are we expecting here?)</li>
