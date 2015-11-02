b0871820e635b4a0417df9a99036ac196149af3a
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-11-02 08:20:44 -0700

    ERD works now.

--- public_html/documentation/entity-relationship.php
+++ public_html/documentation/entity-relationship.php
@@ -5,7 +5,7 @@
 		<title>Entity Relationship Diagram</title>
 	</head>
 	<body>
-		<a href="image/erd-simple-1.svg"> </a>
-		<a href="image/erd-simple-1w-xml.svg" alt="Entity Relationship Diagram"/>
+		<p>Basic Entity Relationship Diagram</p>
+		<img src="image/erd-simple-1w-xml.svg" alt="Entity Relationship Diagram"/>
 	</body>
 </html>
