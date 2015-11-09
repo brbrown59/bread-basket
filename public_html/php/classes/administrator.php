ef57c52baf39b3de9856f178b5fe4f8b4f3c29cb
Carlos Beraun <cberaun2@cnm.edu>
2015-11-09 07:16:59 -0700

    Added accessors and mutators for Amin, Vol, and Org ID's https://app.asana.com/0/60990639151493/60995368417854

--- public_html/php/classes/administrator.php
+++ public_html/php/classes/administrator.php
@@ -67,8 +67,25 @@ class Administrator {
 	 */
 	private $adminSalt;
 
-	public function __construct($newAdminId, $newVolId, $newOrgId, $newAdminEmailId, $newAdminEmailActivation, $newAdminFirstNameId, $newAdminHashId, $newAdminLastNameId, $newAdminPhoneId, $newAdminSaltId){
-		try{
+	/**
+	 * Constructors for this Administrator ID
+	 *
+	 * @param $newAdminId
+	 * @param $newVolId
+	 * @param $newOrgId
+	 * @param $newAdminEmailId
+	 * @param $newAdminEmailActivation
+	 * @param $newAdminFirstNameId
+	 * @param $newAdminHashId
+	 * @param $newAdminLastNameId
+	 * @param $newAdminPhoneId
+	 * @param $newAdminSaltId
+	 * @throws Exception
+	 *
+	 */
+
+	public function __construct($newAdminId, $newVolId, $newOrgId, $newAdminEmailId, $newAdminEmailActivation, $newAdminFirstNameId, $newAdminHashId, $newAdminLastNameId, $newAdminPhoneId, $newAdminSaltId) {
+		try {
 			$this->setAdminId($newAdminId);
 			$this->setVolId($newVolId);
 			$this->SetOrgId($newOrgId);
@@ -78,16 +95,127 @@ class Administrator {
 			$this->SetAdminHashId($newAdminHashId);
 			$this->SetAdminPhoneId($newAdminPhoneId);
 			$this->SetAdminSaltId($newAdminSaltId);
+
+
 		} catch(invalidArgumentException $invalidArgument) {
 			// rethrow the exception to the user
-			throw(new InvalidArgumentException($invalidArgument-> getMessage(), 0, $invalidArgument));
+			throw(new InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+
+
 		} catch(RangeException $range) {
 			// rethrow the exception to the user
 			throw(new RangeException($range->getMessage(), 0, $range));
-		}catch(Exception $exception){
+
+
+		} catch(Exception $exception) {
 			// rethrow generic exception
 			throw(new Exception($exception->getMessage(), 0, $exception));
 		}
+
+	}
+
+	/**
+	 * Accessor method for the Aministrator Id
+	 */
+	Public function getAdminId() {
+		return ($this->adminId);
+	}
+
+	/**Mutator for Administrator ID
+	 * @param Integer ; $newAdminId new value of Administrator Id
+	 * @throw InvalidAgrumentException if the new Administrator Id is not an Integer.
+	 **/
+	Public function setAdminId($newAdminId) {
+		//base case
+		if($newAdminId === null) {
+			$this->adminId = null;
+			return;
+		}
+
+		//Verify the Administrator Id is valid
+		$newAdminId = filter_var($newAdminId, FILTER_VALIDATE_INT);
+		if($newAdminId === false) {
+			throw(new InvalidArgumentException("This Administrator IS is not a valid iteger"));
+		}
+
+		//verify the Administrator ID is positive
+		if($newAdminId <= 0) {
+			throw (new RangeException("This Administrator IDis not positive"));
+		}
+
+		//convert and store the Administrator Id
+		$this->adminId = intval($newAdminId);
+	}
+
+	/**
+	 * Accessor method for the Volunteer Id
+	 * @return integer value of Volunteer Id
+	 */
+	Public function getVolId() {
+		return ($this->volId);
+	}
+
+	/**Mutator for Vol ID
+	 * @param Integer ; $newVolId new value of Volunteer Id
+	 * @throw InvalidArgumentException if the new Volunteer Id is not an Integer.
+	 **/
+	Public function setVolId($newVolId) {
+		//base case
+		if($newVolId === null) {
+			$this->volId = null;
+			return;
+		}
+
+		//Verify the Volunteer Id is valid
+		$newVolId = filter_var($newVolId, FILTER_VALIDATE_INT);
+		if($newVolId === false) {
+			throw(new InvalidArgumentException("This Volunteer IS is not a valid iteger"));
+		}
+
+		//verify the Volunteer ID is positive
+		if($newVolId <= 0) {
+			throw (new RangeException("This Volunteer ID is not positive"));
+		}
+
+		//convert and store the Volunteer Id
+		$this->volId = intval($newVolId);
+	}
+
+	/**
+	 * accessor method for Organization ID
+	 * @return int value of Organization ID
+	 */
+	public function getOrgID() {
+		return ($this->orgId);
+	}
+
+	/**Mutator for Organization Id ID
+	 * @param Integer ; $newOrgId new value of Organization Id
+	 * @throw InvalidAgrumentException if the new Organization Id is not an Integer.
+	 **/
+	Public function setOrgId($newOrgId) {
+		//base case
+		if($newOrgId === null) {
+			$this->adminId = null;
+			return;
+		}
+
+		//Verify the Organization Id is valid
+		$newOrgId = filter_var($newOrgId, FILTER_VALIDATE_INT);
+		if($newOrgId === false) {
+			throw(new InvalidArgumentException("This Organization IS is not a valid iteger"));
+		}
+
+		//verify the Organization ID is positive
+		if($newOrgId <= 0) {
+			throw (new RangeException("This Organization IDis not positive"));
+		}
+
+		//convert and store the Organization Id
+		$this->orgId = intval($newOrgId);
+	}
+
+}
 
 
 ?>
