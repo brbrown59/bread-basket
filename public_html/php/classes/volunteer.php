0a96ba5f1a977ccf98df7d55af697c6b137cbd48
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-10 08:34:59 -0700

    added autoloader in require once https://app.asana.com/0/60993780339150/63422107937638

--- public_html/php/classes/volunteer.php
+++ public_html/php/classes/volunteer.php
@@ -1,6 +1,11 @@
 <?php
 
 /**
+ * autoloader function to include other classes
+ */
+require_once("autoloader.php");
+
+/**
  * Volunteer is a small user attached to a receiving organization. They do not have access to the organization profile
  * They receive notifications about donations and have the ability to claim donations and pickup donations. They are managed
  * by the admin of the organization they're associated with.
