bf487c9cb17c0b6d87162b0f99e2ed1e90f79b05
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-10 09:46:13 -0700

    Added tests for getListingByListingId and getListingByOrgId https://app.asana.com/0/60990639151493/63422106868078

--- test/listing-test.php
+++ test/listing-test.php
@@ -164,4 +164,69 @@ class ListingTest extends BreadBasketTest {
 				$this->VALID_PARENT_ID, $this->VALID_DATETIME, $this->VALID_TYPE);
 		$listing->delete($this->getPDO());
 	}
+
+	/**
+	 * test inserting a listing and regrabbing it from mySQL
+	 */
+	public function testGetValidListingByListingId() {
+		//get the count of the number of rows in the database
+		$numRows = $this->getConnection()->getRowCount("listing");
+
+		//create a new listing and insert into mySQL
+		$listing = new Listing(null, $this->VALID_ORGID, $this->VALID_CLAIMEDBY, $this->VALID_LISTINGCLOSED, $this->VALID_COST, $this->VALID_MEMO,
+				$this->VALID_PARENT_ID, $this->VALID_DATETIME, $this->VALID_TYPE);
+		$listing->insert($this->getPDO());
+
+		//grab data from SQL and ensure it matches
+		$pdoListing = Listing::getListingByListingId($this->getPDO(), $listing->getListingId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("listing"));
+		$this->assertSame($pdoListing->getOrgId(), $this->VALID_ORGID);
+		$this->assertSame($pdoListing->getListingClaimedBy(), $this->VALID_CLAIMEDBY);
+		$this->assertSame($pdoListing->getListingClosed(), $this->VALID_LISTINGCLOSED);
+		$this->assertSame($pdoListing->getListingCost(), $this->VALID_COST);
+		$this->assertSame($pdoListing->getListingMemo(), $this->VALID_MEMO);
+		$this->assertSame($pdoListing->getParentId(), $this->VALID_PARENT_ID);
+		$this->assertSame($pdoListing->getListingPostTime(), $this->VALID_DATETIME);
+		$this->assertSame($pdoListing->getListingTypeId(), $this->VALID_TYPE);
+	}
+
+	/**
+	 * test getting an organization that doesn't exist
+	 */
+	public function testGetInvalidListingByListingId() {
+		$listing = Listing::getListingByListingId($this->getPDO(), BreadBasketTest::INVALID_KEY);
+		$this->assertNull($listing);
+	}
+
+	public function testGetValidListingByOrgId() {
+		//get the count of the number of rows in the database
+		$numRows = $this->getConnection()->getRowCount("listing");
+
+		//create a new listing and insert into mySQL
+		$listing = new Listing(null, $this->VALID_ORGID, $this->VALID_CLAIMEDBY, $this->VALID_LISTINGCLOSED, $this->VALID_COST, $this->VALID_MEMO,
+				$this->VALID_PARENT_ID, $this->VALID_DATETIME, $this->VALID_TYPE);
+		$listing->insert($this->getPDO());
+
+		//grab data from SQL and ensure it matches
+		$pdoListing = Listing::getListingByOrgId($this->getPDO(), $listing->getOrgId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("listing"));
+		$this->assertSame($pdoListing[0]->getOrgId(), $this->VALID_ORGID);
+		$this->assertSame($pdoListing[0]->getListingClaimedBy(), $this->VALID_CLAIMEDBY);
+		$this->assertSame($pdoListing[0]->getListingClosed(), $this->VALID_LISTINGCLOSED);
+		$this->assertSame($pdoListing[0]->getListingCost(), $this->VALID_COST);
+		$this->assertSame($pdoListing[0]->getListingMemo(), $this->VALID_MEMO);
+		$this->assertSame($pdoListing[0]->getParentId(), $this->VALID_PARENT_ID);
+		$this->assertSame($pdoListing[0]->getListingPostTime(), $this->VALID_DATETIME);
+		$this->assertSame($pdoListing[0]->getListingTypeId(), $this->VALID_TYPE);
+	}
+
+	/**
+	 * test for grabbing a listing by an orgid that doesn't exist
+	 */
+	public function testGetInvalidListingByOrgId() {
+		$listing = Listing::getListingByOrgId($this->getPDO(), 1);
+		$this->assertSame($listing->getSize(), 0);
+	}
+
+
 }
