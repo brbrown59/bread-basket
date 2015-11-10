a5a8cecc40300905d11ffbfaa37c2e8f8ab2c2e2
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-09 23:16:18 -0700

    trying to get foriegn keys to work

--- test/volunteer-test.php
+++ test/volunteer-test.php
@@ -65,6 +65,7 @@ class VolunteerTest extends BreadBasketTest {
 		//grab the data from mySQL and enforce the fields match our expectations
 		$pdoVolunteer = Volunteer::getVolunteerByVolId($this->getPDO(), $volunteer->getVolId());
 		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("volunteer"));
+		$this->assertSame($pdoVolunteer->getOrgId(), $this->VALID_ORG_ID);
 		$this->assertSame($pdoVolunteer->getVolEmail(), $this->VALID_EMAIL);
 		$this->assertSame($pdoVolunteer->getVolEmailActivation(), $this->VALID_EMAIL_ACTIVATION);
 		$this->assertSame($pdoVolunteer->getVolFirstName(), $this->VALID_FIRST_NAME);
