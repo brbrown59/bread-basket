7f2894008f471ec4cb5cec9ca26a356dc198e486
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-11-10 15:51:41 -0700

    Changes line 227 again listing by a parent id on Unit Tests https://app.asana.com/0/60993780339172/63422106868078

--- test/listing-test.php
+++ test/listing-test.php
@@ -274,8 +274,8 @@ protected $organization = null;
 	 * test for grabbing a listing by a parent id that doesn't exist
 	 */
 	public function testGetInvalidListingParentId() {
-		$listing = Listing::getListingByListingParentId($this->getPDO(), BreadBasketTest::INVALID_KEY);
-		$this->assertNull($listing);
+		$listing = Listing::getListingByListingParentId($this->getPDO(), "100000000000000000000");
+		$this->assertSame($listing->getSize(), 0);
 	}
 
 	/**test for grabbing a listing by listingPostTime
