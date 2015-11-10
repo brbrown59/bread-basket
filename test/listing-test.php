b30bb04584f8e913f36b72818a6e39e779ba39aa
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-10 09:29:38 -0700

    Added tests for insert, update, and delete https://app.asana.com/0/60990639151493/63422106868078

--- test/listing-test.php
+++ test/listing-test.php
@@ -36,6 +36,11 @@ class ListingTest extends BreadBasketTest {
 	 **/
 	protected $VALID_COST = 94.25;
 	/**
+	 * a second valid listing cost, for updating
+	 * @var Double $VALID_COST_2
+	 */
+	protected $VALID_COST_2 = 211.53;
+	/**
 	 * valid listing memo
 	 * @var String $VALID_MEMO
 	 **/
@@ -45,16 +50,17 @@ class ListingTest extends BreadBasketTest {
 	 * @var Int $VALID_PARENT_ID
 	 */
 	protected $VALID_PARENT_ID = "5554268";
-/**
- * valid listingPostTime
- * @var DATETIME $VALID_DATETIME
- **/
+	/**
+	 * valid listingPostTime
+	 * @var DATETIME $VALID_DATETIME
+	 **/
 	protected $VALID_DATETIME = "20115-02-15 18:25:00";
 	/**
 	 * valid listing type to use
 	 * @var Int $VALID_TYPE
 	 **/
 	protected $VALID_TYPE = "3";
+
 	/**
 	 * test inserting a valid listing and verify that the actual mySQL data matches
 	 **/
@@ -63,20 +69,99 @@ class ListingTest extends BreadBasketTest {
 		$numRows = $this->getConnection()->getRowCount("listing");
 
 	//create a new listing and insert into mySQL
-$listing = new Listing(null, $this->VALID_ORGID, $this->VALID_CLAIMEDBY, $this->VALID_LISTINGCLOSED, $this->VALID_COST, $this->VALID_MEMO,
-$this->VALID_PARENT_ID, $this->VALID_DATETIME, $this->VALID_TYPE);
-$listing->insert($this->getPDO());
+		$listing = new Listing(null, $this->VALID_ORGID, $this->VALID_CLAIMEDBY, $this->VALID_LISTINGCLOSED, $this->VALID_COST, $this->VALID_MEMO,
+				$this->VALID_PARENT_ID, $this->VALID_DATETIME, $this->VALID_TYPE);
+		$listing->insert($this->getPDO());
 
-	//grab data from SQL and ensure it matches
-$pdoListing = Listing::getListingByListingId($this->getPDO(), $listing->getListingId());
-$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("listing"));
-$this->assertSame($pdoListing->getOrgId(), $this->VALID_ORGID);
-$this->assertSame($pdoListing->getListingClaimedBy(), $this->VALID_CLAIMEDBY);
-$this->assertSame($pdoListing->getListingClosed(), $this->VALID_LISTINGCLOSED);
-$this->assertSame($pdoListing->getListingCost(), $this->VALID_COST);
-$this->assertSame($pdoListing->getListingMemo(), $this->VALID_MEMO);
-$this->assertSame($pdoListing->getParentId(), $this->VALID_PARENT_ID);
-$this->assertSame($pdoListing->getListingPostTime(), $this->VALID_DATETIME);
-$this->assertSame($pdoListing->getListingTypeId(), $this->VALID_TYPE);
-}
+		//grab data from SQL and ensure it matches
+		$pdoListing = Listing::getListingByListingId($this->getPDO(), $listing->getListingId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("listing"));
+		$this->assertSame($pdoListing->getOrgId(), $this->VALID_ORGID);
+		$this->assertSame($pdoListing->getListingClaimedBy(), $this->VALID_CLAIMEDBY);
+		$this->assertSame($pdoListing->getListingClosed(), $this->VALID_LISTINGCLOSED);
+		$this->assertSame($pdoListing->getListingCost(), $this->VALID_COST);
+		$this->assertSame($pdoListing->getListingMemo(), $this->VALID_MEMO);
+		$this->assertSame($pdoListing->getParentId(), $this->VALID_PARENT_ID);
+		$this->assertSame($pdoListing->getListingPostTime(), $this->VALID_DATETIME);
+		$this->assertSame($pdoListing->getListingTypeId(), $this->VALID_TYPE);
+	}
+
+	/**
+	 * test inserting a listing that cannot be added
+	 *
+	 * @expectedException PDOException
+	 */
+	public function testInsertInvalidListing() {
+		//create listing with non-null id, and hope it fails
+		$listing = new Listing(BreadBasketTest::INVALID_KEY, $this->VALID_ORGID, $this->VALID_CLAIMEDBY, $this->VALID_LISTINGCLOSED, $this->VALID_COST, $this->VALID_MEMO,
+				$this->VALID_PARENT_ID, $this->VALID_DATETIME, $this->VALID_TYPE);
+		$listing->insert($this->getPDO());
+	}
+
+	public function testUpdateValidListing() {
+		//get the count of the number of rows in the database
+		$numRows = $this->getConnection()->getRowCount("listing");
+
+		//create a new listing and insert into mySQL
+		$listing = new Listing(null, $this->VALID_ORGID, $this->VALID_CLAIMEDBY, $this->VALID_LISTINGCLOSED, $this->VALID_COST, $this->VALID_MEMO,
+				$this->VALID_PARENT_ID, $this->VALID_DATETIME, $this->VALID_TYPE);
+		$listing->insert($this->getPDO());
+
+		//edit the listing and update in mySQL
+		$listing->setListingCost($this->VALID_COST_2);
+		$listing->update($this->getPDO());
+
+		//grab data from SQL and ensure it matches
+		$pdoListing = Listing::getListingByListingId($this->getPDO(), $listing->getListingId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("listing"));
+		$this->assertSame($pdoListing->getOrgId(), $this->VALID_ORGID);
+		$this->assertSame($pdoListing->getListingClaimedBy(), $this->VALID_CLAIMEDBY);
+		$this->assertSame($pdoListing->getListingClosed(), $this->VALID_LISTINGCLOSED);
+		$this->assertSame($pdoListing->getListingCost(), $this->VALID_COST_2);
+		$this->assertSame($pdoListing->getListingMemo(), $this->VALID_MEMO);
+		$this->assertSame($pdoListing->getParentId(), $this->VALID_PARENT_ID);
+		$this->assertSame($pdoListing->getListingPostTime(), $this->VALID_DATETIME);
+		$this->assertSame($pdoListing->getListingTypeId(), $this->VALID_TYPE);
+	}
+
+	/**
+	 * test updating an listing that does not exist
+	 *
+	 * @expectedException PDOException
+	 */
+	public function testUpdateInvalidListing() {
+		//create listing with non-null id, and hope it fails
+		$listing = new Listing(null, $this->VALID_ORGID, $this->VALID_CLAIMEDBY, $this->VALID_LISTINGCLOSED, $this->VALID_COST, $this->VALID_MEMO,
+				$this->VALID_PARENT_ID, $this->VALID_DATETIME, $this->VALID_TYPE);
+		$listing->update($this->getPDO());
+	}
+
+	public function testDeleteValidListing() {
+		//get the count of the number of rows in the database
+		$numRows = $this->getConnection()->getRowCount("listing");
+
+		//create a new listing and insert into mySQL
+		$listing = new Listing(null, $this->VALID_ORGID, $this->VALID_CLAIMEDBY, $this->VALID_LISTINGCLOSED, $this->VALID_COST, $this->VALID_MEMO,
+				$this->VALID_PARENT_ID, $this->VALID_DATETIME, $this->VALID_TYPE);
+		$listing->insert($this->getPDO());
+
+		//delete the listing in mySQL
+		$listing->delete($this->getPDO());
+
+		//grab data from SQL and ensure it matches
+		$pdoListing = Listing::getListingByListingId($this->getPDO(), $listing->getListingId());
+		$this->assertNull($pdoListing);
+		$this->assertSame($numRows, $this->getConnection()->getRowCount("listing"));
+	}
+
+	/**
+	 * test deleting a listing that does not exist
+	 *
+	 * @expectedException PDOException
+	 */
+	public function testDeleteInvalidListing() {
+		$listing = new Listing(null, $this->VALID_ORGID, $this->VALID_CLAIMEDBY, $this->VALID_LISTINGCLOSED, $this->VALID_COST, $this->VALID_MEMO,
+				$this->VALID_PARENT_ID, $this->VALID_DATETIME, $this->VALID_TYPE);
+		$listing->delete($this->getPDO());
+	}
 }
