43f20811220922bda0a33ec007870d175a9e1fb3
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-11 13:55:05 -0700

    added some containers https://app.asana.com/0/60993780339150/63421877783062

--- public_html/php/template/detail-view.php
+++ public_html/php/template/detail-view.php
@@ -58,20 +58,42 @@
 			<main>
 				<div class="container">
 					<div class="row">
-						<div class="col-md-6">
+						<div class="col-md-12">
 							<h1>Details</h1>
 						</div>
-						<div class="col-md-6">
+					</div>
+					<div class="row">
+						<div class="col-md-12">
 							<button type="button" class="btn btn-default">
 								<a href="#">
 									<span class="glyphicon glyphicon-triangle-left"></span>
 									All Listings
 								</a>
 							</button>
-
 						</div>
 					</div>
 				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="text-box">
+								<h3>one</h3>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="text-box">
+								<h3>one</h3>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="text-box">
+								<h3>one</h3>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
 			</main>
 		</div>
 
