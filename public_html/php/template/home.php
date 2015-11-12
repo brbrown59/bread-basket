6d4e06c7cf8c80b651331804669047e986b28a7d
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-12 16:32:01 -0700

    added angular and boostrap cdns https://app.asana.com/0/60993780339150/63421877783062

---
+++ public_html/php/template/home.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+* Angular version
+**/
+$ANGULAR_VERSION = "1.4.7";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- CSS stylesheets -->
+		<!--latest compiled and minified bootstrap css files-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		<!--optional theme-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+		<!--minified font awesome css-->
+		<link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+		<!-- CUSTOM stylesheets -->
+
+		<!-- CDN derived JavaScript -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs<?php $ANGULAR_VERSION; ?>/angular.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php $ANGULAR_VERSION; ?>/angular-messages.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.14.3/ui-bootstrap-tpls.min.js"></script>
+
+
+	</head>
+</html>
