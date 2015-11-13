f2471ca4b7cc3d00899ceab19e3f9c43e96efe47
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-13 10:16:38 -0700

    starting home page https://app.asana.com/0/60993780339150/63421877783062

--- public_html/php/template/home.php
+++ public_html/php/template/home.php
@@ -9,6 +9,9 @@ $ANGULAR_VERSION = "1.4.7";
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<!-- CSS stylesheets -->
 		<!--latest compiled and minified bootstrap css files-->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
@@ -17,12 +20,66 @@ $ANGULAR_VERSION = "1.4.7";
 		<!--minified font awesome css-->
 		<link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
 		<!-- CUSTOM stylesheets -->
+		<link type="text/css" rel="stylesheet" href="/public_html/css/custom-style.css"/>
 
 		<!-- CDN derived JavaScript -->
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs<?php $ANGULAR_VERSION; ?>/angular.min.js"></script>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php $ANGULAR_VERSION; ?>/angular-messages.min.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.14.3/ui-bootstrap-tpls.min.js"></script>
 
+		<title>Bread Basket Home</title>
 
 	</head>
+
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<div class="container-fluid">
+					<!--begin navbar-->
+					<nav class="nav navbar-default">
+						<!--logo and mobile toggle button get grouped together-->
+						<div class="navbar-header">
+							<button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#my-navbar" aria-expanded="false">
+								<span class="sr-only">Main Menu</span>
+								<span class="glyphicon glyphicon-th-large"></span>
+							</button>
+							<a href="#" class="navbar-brand">
+								<span class="glyphicon glyphicon-grain"></span>
+								Bread Basket
+							</a>
+						</div>
+
+						<!--nav links are grouped together here-->
+						<div class="collapse navbar-collapse navbar-right" id="my-navbar">
+							<ul class="nav navbar-nav">
+								<li><a href="#">Home</a></li>
+								<li><a href="#">Listings</a></li>
+								<li><a href="#">Profile</a></li>
+								<li><a href="#">Volunteers</a> </li>
+								<li><a href="#">Help</a></li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</header>
+			<!--main content-->
+			<main>
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="text-center lead">
+								Connecting People To End Hunger
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+
+			</main>
+
+		</div>
+	</body>
+
+
 </html>
