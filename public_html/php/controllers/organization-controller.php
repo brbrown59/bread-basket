1bdbcbfb4304024ef1908f460dfee82cc0e03522
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-13 12:11:41 -0700

    added code for creating a pusher connection and (I think?) checking if a volunteer is actually logged in https://app.asana.com/0/60990639151493/65845316003349

--- public_html/php/controllers/organization-controller.php
+++ public_html/php/controllers/organization-controller.php
@@ -10,20 +10,37 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
  *
  * @author Bradley Brown <tall.white.ninja@gmail.com>
  */
-
-//start the session and create a token
-if(session_status() !== PHP_SESSION_ACTIVE) {
-	session_start();
-}
-
-//prepare an empty reply
-$reply = new stdClass();
-$reply->status = 200;
-$reply->data = null;
-
 try {
 
+	//verify the xsrf challenge
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
+	//if the session is empty, throw an exception
+	//might put this in session status instead?
+	if(empty($_SESSION["volunteer"]) == true) {
+		throw(new RuntimeException("Please log-in or sign up"));
+	}
+
+	verifyXsrf();
+
+	//prepare an empty pusher reply
+	$reply = new stdClass();
+	$reply->status = 200;
+	$reply->data = null;
+
+	//create the pusher connection
+	//make sure pusher details get put into the config file!!!!
+	//does EVERY class need to push things (I assume so)?
+	$config = readConfig("/etc/apache2/capstone-mysql/breadbasket.ini");
+	$pusherConfig = json_decode($config["pusher"]);
+	$pusher = new Pusher($pusherConfig->key, $pusherConfig->secret, $pusherConfig->id, ["encrypted" => true]);
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
 } catch(Exception $exception) {
+	//send exception back to the caller
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 }
