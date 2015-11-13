2de3edcc1be127071b36ceb89fc6bb10e9291271
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-13 10:54:13 -0700

    Coded the generic set-up and exception handling portions https://app.asana.com/0/60990639151493/65845316003349

--- public_html/php/controllers/organization-controller.php
+++ public_html/php/controllers/organization-controller.php
@@ -1,11 +1,34 @@
 <?php
 
-
-require_once "../classes/organization.php";
-
+//autoload?
+require_once dirname(__DIR__)."/classes/organization.php";
+require_once dirname(__DIR__)."lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 /**
  * controller/api for the organization class
  *
  * @author Bradley Brown <tall.white.ninja@gmail.com>
  */
+
+//start the session and create a token
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+header("Content-type: application/json");
+if($reply->data == null) {
+	unset($reply->data);
+}
+echo json_encode($reply);
