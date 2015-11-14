84fa8545399e3c7eae79beb6b9f7c17c0547cf7f
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-11-14 09:53:04 -0700

    Reworked sign in for json. https://app.asana.com/0/60993780339172/65870660292100

--- public_html/php/controllers/login-controller.php
+++ public_html/php/controllers/login-controller.php
@@ -1,8 +1,8 @@
 <?php
-//autoloads classes
+//auto loads classes
 require_once(dirname(dirname(__DIR__)) . "/php/classes/autoloader.php");
 //security w/ NG in mind
-require_once dirname(__DIR__)."lib/xsrf.php";
+require_once dirname(__DIR__) . "lib/xsrf.php";
 //a security file that's on the server created by Dylan
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
@@ -10,31 +10,44 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
  * controller for the logging in
  *
  * @author Tamra Fenstermaker <fenstermaker505@gmail.com
- * contributing code from TruFork https://github.com/Skylarity/trufork
+ * contributing code from TruFork https://github.com/Skylarity/trufork & foodinventory
  */
-try {
-	//ensures that the fields are filled out
-	if(@isset($_POST["volEmail"]) === false || @isset($_POST["Password"]) === false) {
-		throw(new InvalidArgumentException("form not complete. Please verify and try again"));
-	}
-	// verify the XSRF challenge
-	if(session_status() !== PHP_SESSION_ACTIVE) {
-		session_start();
-	}
-	verifyXsrf();
-	// create a salt and hash for user
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/breadbasket.ini");
-	$volunteer = Volunteer::getVolunteerByVolEmail($pdo, $_POST["volEmail"]);
-	if($volunteer === null) {
-		throw(new InvalidArgumentException("email or password is invalid"));
-	}
-	$volHash = hash_pbkdf2("sha512", $_POST["Password"], $volunteer->getVolSalt(), 262144, 128);
-	if($volHash !== $volunteer->getVolHash()) {
-		throw(new InvalidArgumentException("email or password is invalid"));
-	}
-	$_SESSION["volunteer"] = $volunteer;
-	$volFirstName = $_SESSION["volunteer"]->getVolFirstName();
-	echo "<p class=\"alert alert-success\">Welcome Back, " . $volFirstName . "!<p/>";
-} catch(Exception $exception) {
-	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
+
+//start the session and create a XSRF token
+if(session_start() !== PHP_SESSION_ACTIVE) {
+	session_start();
 }
+
+// prepare default error message
+$reply = new stdClass();
+$reply->status = 401;
+$reply->message = "Username/password incorrect";
+
+try {
+	// grab the my SQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/breadbasket.ini");
+
+	// convert POSTed JSON to an object
+	verifyXsrf();
+	$requestContent = file_get_contents("php://input");
+	$requestObject = json_decode($requestContent);
+
+	// sanitize the email & search by volEmail
+	$volEmail = filter_var($requestObject->volEmail, FILTER_SANITIZE_EMAIL);
+	$volunteer = Volunteer::getVolunteerByVolEmail($pdo, $volEmail);
+
+	if($volunteer !== null) {
+		$volHash = hash_pbkdf2("sha512", $requestObject->password, $volunteer->getVolSalt(), 262144, 128);
+		if($volHash === $volunteer->getVolHash()) {
+			$_SESSION["volunteer"] = $volunteer;
+			$reply->status = 200;
+			$reply->message = "User logged in";
+		}
+	}
+	// create an exception to pass bak to the RESTfull caller
+}catch (Exception $exception) {
+	// ignore them ToDo QUESTION does "them" mean the exception?
+}
+
+header("Content-type: application/json");
+echo json_encode($reply);
