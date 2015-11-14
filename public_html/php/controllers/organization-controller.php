555338f9b34a9dab0d1f8adfba3c016d04432b64
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-14 15:50:54 -0700

    Implemented the checks for volunteer/admin logins https://app.asana.com/0/60990639151493/65845316003349

--- public_html/php/controllers/organization-controller.php
+++ public_html/php/controllers/organization-controller.php
@@ -16,9 +16,10 @@ try {
 	if(session_status() !== PHP_SESSION_ACTIVE) {
 		session_start();
 	}
-	//if the session is empty, throw an exception
-	//might put this in session status instead?
-	if(empty($_SESSION["volunteer"]) == true) {
+	//if the volunteer session is empty, and the user is not logged in, throw an exception
+	//note: this may be redundant with some logic below, and is this what we should check?
+	//also, ensure that BOTH volunteer and admin are set for an admin instance, rather than just one or the other
+	if(empty($_SESSION["volunteer"]) === true) {
 		throw(new RuntimeException("Please log-in or sign up"));
 	}
 
@@ -38,6 +39,32 @@ try {
 
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//TODO: possible input sanitization? How do I know for sure what I'm getting (ex: id vs city?)
+
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/encrypted-config.ini");
+
+	//handle REST calls, while only allowing administrators access to database-modifying methods
+	if(empty($_SESSION["volunteer"]) === false) { //note: is this redundant with the check above?
+		if($method === "GET") {
+			//set XSRF cookie
+			setXsrfCookie("/");
+			//TODO: this is where all of the get foo by bar functions are going, since they can get on several fields
+			//TODO: also, at end of if/else block will be a base case of get everything
+		}
+	}
+	//if the session belongs to an admin, allow post, put, and delete methods
+	if(empty($_SESSION["admin"]) === false) {
+		//POST, PUT, DELETE here
+
+	} else {
+		//if not an admin, and attempting a method other than get, throw an exception
+		if((empty($method) === false) && ($method !== "GET")) {
+			throw new RuntimeException("Only administrators are allowed to modify entries");
+		}
+	}
+
 
 } catch(Exception $exception) {
 	//send exception back to the caller
