68ab7d8720acd192a3af86c41c22aa01adc72187
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-16 12:01:53 -0700

    added separate nav page https://app.asana.com/0/60993780339150/63421877783062

---
+++ public_html/php/template/nav.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<title>Listing Details</title>
+
+		<!--latest compiled and minified bootstrap css files-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		<!--optional theme-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+		<!--bread basket custom stylesheets-->
+		<link rel="stylesheet" href="../../css/custom-style.css"/>
+
+		<!--HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries-->
+		<!--WARNING: Respond.js doesn't work if you view the page via file://-->
+		<!--[if lt IE 9]>
+		<script src=//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<div class="container-fluid">
+					<!--begin navbar-->
+					<nav class="nav navbar-default">
+						<!--logo and mobile toggle button get grouped together-->
+						<div class="navbar-header">
+							<button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#my-navbar" aria-expanded="false">
+								<span class="sr-only">Main Menu</span>
+								<span class="glyphicon glyphicon-th-large"></span>
+							</button>
+							<a href="#" class="navbar-brand">
+								<span class="glyphicon glyphicon-grain"></span>
+								Bread Basket
+							</a>
+						</div>
+
+						<!--nav links are grouped together here-->
+						<div class="collapse navbar-collapse navbar-right" id="my-navbar">
+							<ul class="nav navbar-nav">
+								<li><a href="#">Home</a></li>
+								<li><a href="#">Listings</a></li>
+								<li><a href="#">Profile</a></li>
+								<li><a href="#">Volunteers</a> </li>
+								<li><a href="#">Help</a></li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</header>
+		</div>
+	</body>
+</html>
