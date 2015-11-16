d040f32024b71dc39a0c379bc70d0d6d4cd0dc4d
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-16 15:20:18 -0700

    TABS ARE WORKING also edited a typo in container 2  https://app.asana.com/0/60993780339150/63421877783062

--- public_html/php/template/home.php
+++ public_html/php/template/home.php
@@ -95,7 +95,7 @@ $ANGULAR_VERSION = "1.4.6";
 					</div>
 				</div>
 				<!--container 2-->
-				<div class="contianer-fluid">
+				<div class="container-fluid">
 					<div class="padding-top-bottom-sm">
 						<div class="row">
 							<div class="col-md-12 center-block">
@@ -109,7 +109,7 @@ $ANGULAR_VERSION = "1.4.6";
 							<div class="col-md-4 col-md-offset-4">
 								<div ng-controller="TabsController">
 									<uib-tabset justified="true">
-										<uib-tab ng-repeat="tab in tabs" heading="{{tab.title}}" ng-click="tabs[1].active = true" active="tab.active" disable="tab.disabled">
+										<uib-tab ng-repeat="tab in tabs" heading="{{tab.title}}" >
 											{{tab.content}}
 										</uib-tab>
 									</uib-tabset>
