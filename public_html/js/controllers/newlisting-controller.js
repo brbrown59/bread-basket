568b6c4c521c1592ffaea4dfdc19d0f2630310c8
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-17 15:17:09 -0700

    minor edit https://app.asana.com/0/60993780339150/63421877783062

--- public_html/js/controllers/newlisting-controller.js
+++ public_html/js/controllers/newlisting-controller.js
@@ -3,8 +3,8 @@ app.controller("NewListingController", ["$scope", "$uibModal", function($scope, 
 
 	$scope.openListingModal = function () {
 		var ListingModalInstance = $uibModal.open({
-			templateUrl: "../../js/views/signup-modal.php",
-			controller: "SignupModal",
+			templateUrl: "../../js/views/newlisting-modal.php",
+			controller: "ListingModal",
 			resolve: {
 				signinData : function () {
 					return($scope.listingData);
