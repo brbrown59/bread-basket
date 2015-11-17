806126616349ea5accf95f28353582e161466f68
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-17 15:14:34 -0700

    adding modal to all listings page https://app.asana.com/0/60993780339150/63421877783062

--- public_html/php/template/listing-view.php
+++ public_html/php/template/listing-view.php
@@ -1,29 +1,42 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="BreadBasket">
 	<head>
 		<meta charset="utf-8"/>
 		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-		<title>Listing Details</title>
-
+		<!-- CSS stylesheets -->
 		<!--latest compiled and minified bootstrap css files-->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		<link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
 		<!--optional theme-->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
-		<!--bread basket custom stylesheets-->
-		<link rel="stylesheet" href="../../css/custom-style.css"/>
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+		<!--minified font awesome css-->
+		<link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+		<!-- CUSTOM stylesheets -->
+		<link type="text/css" rel="stylesheet" href="../../css/custom-style.css"/>
 
-		<!--HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries-->
-		<!--WARNING: Respond.js doesn't work if you view the page via file://-->
-		<!--[if lt IE 9]>
-		<script src=//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
 		<!--jQuery for Bootstrap's .js plugins-->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 		<!--latest compiled and minified bootstrap javascript-->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
+		<!--CDN derived Angular.js -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular-messages.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.14.3/ui-bootstrap-tpls.min.js"></script>
+
+		<!-- CUSTOM js-->
+		<script type="text/javascript" src="../../js/angular-password.min.js"></script>
+		<script type="text/javascript" src="../../js/breadbasket.js"></script>
+		<script type="text/javascript" src="../../js/controllers/tabs.js"></script>
+		<script type="text/javascript" src="../../js/controllers/newlisting-modal.js"></script>
+		<script type="text/javascript" src="../../js/controllers/newlisting-controller.js"></script>
+
+
+		<title>All Listings</title>
+
 	</head>
 	<body class="sfooter">
 		<div class="sfooter-content">
@@ -61,9 +74,9 @@
 				<!--this container houses the h1 tag/headline and the back to listing button-->
 				<div class="container">
 					<div class="row">
-						<div class="col-md-12">
+						<div class="col-md-12" ng-controller="ListingController">
 							<h1 class="inline">All Listings</h1>
-							<button class="btn btn-info pull-right">New Listing</button>
+							<button class="btn btn-info pull-right" ng-click="openListingModal();">New Listing</button>
 						</div>
 					</div>
 				</div>
