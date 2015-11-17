610687c5f29c9e8b4b028e91ec13498091f1f19b
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-11-17 15:45:10 -0700

    Removed Administrator class so unit test will pass for getVolunteerByEmailActivation https://app.asana.com/0/60993780339172/65867904606358

--- public_html/php/api/message/index.php
+++ public_html/php/api/message/index.php
@@ -6,7 +6,6 @@
  */
 
 		//grab the class under scrutiny
-		require_once(dirname(__DIR__) . "/public_html/php/classes/administrator.php");
 		require_once(dirname(__DIR__) . "/public_html/php/classes/organization.php");
 		require_once(dirname(__DIR__) . "/public_html/php/classes/volunteer.php");
 		require_once ("/etc/apache2/data-design/encrypted-config.php");
