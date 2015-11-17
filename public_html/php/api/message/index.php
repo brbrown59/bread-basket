772936f5154d3b0cb9d50ed4b6a894803040cd4f
Carlos Beraun <cberaun2@cnm.edu>
2015-11-17 11:02:49 -0700

    Added "grab class scrutiny" and "create token" blocks (api/message.index.php) https://app.asana.com/0/60990639151493/65845291756892

--- public_html/php/api/message/index.php
+++ public_html/php/api/message/index.php
@@ -1,3 +1,24 @@
 <?php
 
+		//grab the class under scrutiny
+		require_once(dirname(__DIR__) . "/public_html/php/classes/administrator.php");
+		require_once(dirname(__DIR__) . "/public_html/php/classes/organization.php");
+		require_once(dirname(__DIR__) . "/public_html/php/classes/volunteer.php");
+		require_once ("/etc/apache2/data-design/encrypted-config.php");
+
+		//start the session and create a XSRF token
+		if(session_status() !== PHP_SESSION_ACTIVE) {
+			session_start();
+		}
+
+		//prepare an empty reply
+		$reply = new stdClass();
+		$reply->status = 200;
+		$reply->data = null;
+
+		try{
+
+		}
+
+
 ?>
