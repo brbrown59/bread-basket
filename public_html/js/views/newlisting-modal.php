aa26cdd9602a148a633d208effff71f878656a91
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-18 15:48:45 -0700

    added modal body class to forms to correct padding issue https://app.asana.com/0/60993780339150/63421877783062

--- public_html/js/views/newlisting-modal.php
+++ public_html/js/views/newlisting-modal.php
@@ -1,46 +1,48 @@
-<form class="form-horizontal well" id="listingForm" name="listingForm">
-	<h2>Post New Listing</h2>
-	<hr />
-	<p>Please include a pickup time and approximate size of donation in description. <br> Thank you for donating!</p>
-	<!--begin new listing-->
-	<!--memo-->
-	<div class="form-group" ng-class="{ 'has-error' : listingForm.memo.$touched && listingForm.memo.$invalid }">
-		<label class="control-label" for="memo">Description</label>
-		<div class="input-group">
-			<div class="input-group-addon">
-				<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-			</div>
-			<textarea class="form-control" id="memo" name="memo" placeholder="Please add a brief description of your donation" ng-model="listingData.memo" ng-required="true"></textarea>
-		</div>
-		<div class="alert alert-danger" role="alert" ng-messages="listingForm.memo.$error" ng-if="listingForm.memo.$touched" ng-hide="listingForm.memo.$valid">
-			<p ng-message="required">Please add a description</p>
-		</div>
-	</div>
-	<!--cost and type-->
-		<div class="form-group" ng-class="{ 'has-error' : listingForm.cost.$touched && listingForm.cost.$invalid }">
-			<label class="control-label" for="cost">Estimated Cost</label>
+<div class="modal-body">
+	<form class="form-horizontal well" id="listingForm" name="listingForm">
+		<h2>Post New Listing</h2>
+		<hr />
+		<p>Please include a pickup time and approximate size of donation in description. <br> Thank you for donating!</p>
+		<!--begin new listing-->
+		<!--memo-->
+		<div class="form-group" ng-class="{ 'has-error' : listingForm.memo.$touched && listingForm.memo.$invalid }">
+			<label class="control-label" for="memo">Description</label>
 			<div class="input-group">
 				<div class="input-group-addon">
-					<span class="glyphicon glyphicon-usd" aria-hidden="true"></span>
+					<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
 				</div>
-				<input type="number" class="form-control" id="cost" name="cost" placeholder="Estimated cost of donation" min="0" step="0.01" ng-min="0" ng-model="listingData.cost" ng-required="false"/>
+				<textarea class="form-control" id="memo" name="memo" placeholder="Please add a brief description of your donation" ng-model="listingData.memo" ng-required="true"></textarea>
+			</div>
+			<div class="alert alert-danger" role="alert" ng-messages="listingForm.memo.$error" ng-if="listingForm.memo.$touched" ng-hide="listingForm.memo.$valid">
+				<p ng-message="required">Please add a description</p>
 			</div>
 		</div>
-		<!--type-->
-		<div class="radio" ng-class="{ 'has-error' : listingForm.type1.$touched && listingForm.type1.$invalid }">
-			<label class="control-label" for="type1">
-				<input type="radio" name="listingType" id="perishable" value="1" ng-model="listingData.listingType">
-				Perishable
-			</label>
-		</div>
-		<div class="radio" ng-class="{ 'has-error' : listingForm.type2.$touched && listingForm.type2.$invalid }">
-			<label class="control-label" for="type2">
-				<input type="radio" name="listingType" id="nonPerishable" value="2" ng-model="listingData.listingType">
-				Non-Perishable
-			</label>
-		</div>
+		<!--cost and type-->
+			<div class="form-group" ng-class="{ 'has-error' : listingForm.cost.$touched && listingForm.cost.$invalid }">
+				<label class="control-label" for="cost">Estimated Cost</label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<span class="glyphicon glyphicon-usd" aria-hidden="true"></span>
+					</div>
+					<input type="number" class="form-control" id="cost" name="cost" placeholder="Estimated cost of donation" min="0" step="0.01" ng-min="0" ng-model="listingData.cost" ng-required="false"/>
+				</div>
+			</div>
+			<!--type-->
+			<div class="radio" ng-class="{ 'has-error' : listingForm.type1.$touched && listingForm.type1.$invalid }">
+				<label class="control-label" for="type1">
+					<input type="radio" name="listingType" id="perishable" value="1" ng-model="listingData.listingType">
+					Perishable
+				</label>
+			</div>
+			<div class="radio" ng-class="{ 'has-error' : listingForm.type2.$touched && listingForm.type2.$invalid }">
+				<label class="control-label" for="type2">
+					<input type="radio" name="listingType" id="nonPerishable" value="2" ng-model="listingData.listingType">
+					Non-Perishable
+				</label>
+			</div>
 
-	<hr />
-	<button type="submit" class="btn btn-lg btn-info" ng-click="ok();" ng-disabled="listingForm.$invalid"><i class="fa fa-check" aria-hidden="true"></i>Submit</button>
-	<button type="reset" class="btn btn-lg btn-warning" ng-click="cancel();"><i class="fa fa-ban" aria-hidden="true"></i> Cancel</button>
-</form>
+		<hr />
+		<button type="submit" class="btn btn-lg btn-info" ng-click="ok();" ng-disabled="listingForm.$invalid"><i class="fa fa-check" aria-hidden="true"></i>Submit</button>
+		<button type="reset" class="btn btn-lg btn-warning" ng-click="cancel();"><i class="fa fa-ban" aria-hidden="true"></i> Cancel</button>
+	</form>
+</div>
--- public_html/js/views/newvolunteer-modal.php
+++ public_html/js/views/newvolunteer-modal.php
@@ -1,63 +1,65 @@
-<form class="form-horizontal well" id="volunteerForm" name="volunteerForm">
-	<h3>Create New Volunteer</h3>
-	<hr/>
-	<!--begin new volunteer-->
-	<!--first name-->
-	<label class="control-label" for="firstname">Name</label>
-	<div class="form-inline form-group-lg" ng-class="{ 'has-error' : volunteerForm.firstname.$touched && volunteerForm.firstname.$invalid }">
-		<label class="control-label sr-only" for="firstname">Name</label>
-		<div class="input-group">
-			<div class="input-group-addon">
-				<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+<div class="modal-body">
+	<form class="form-horizontal well" id="volunteerForm" name="volunteerForm">
+		<h3>Create New Volunteer</h3>
+		<hr/>
+		<!--begin new volunteer-->
+		<!--first name-->
+		<label class="control-label" for="firstname">Name</label>
+		<div class="form-inline form-group-lg" ng-class="{ 'has-error' : volunteerForm.firstname.$touched && volunteerForm.firstname.$invalid }">
+			<label class="control-label sr-only" for="firstname">Name</label>
+			<div class="input-group">
+				<div class="input-group-addon">
+					<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+				</div>
+				<input type="text" class="form-control" id="firstname" name="firstname" placeholder="First Name" ng-model="volData.firstname" ng-required="true">
 			</div>
-			<input type="text" class="form-control" id="firstname" name="firstname" placeholder="First Name" ng-model="volData.firstname" ng-required="true">
-		</div>
-		<div class="alert alert-danger" role="alert" ng-messages="volunteerForm.firstname.$error" ng-if="volunteerForm.firstname.$touched" ng-hide="volunteerForm.firstname.$valid">
-			<p ng-message="required">Please enter a first name</p>
-		</div>
-		<!--last name-->
-		<label class="control-label sr-only" for="lastname">Last Name</label>
-		<input type="text" class="form-control" id="lastname" name="lastname" placeholder="Last Name" ng-model="volData.lastname" ng-required="true">
-		<div class="alert alert-danger" role="alert" ng-messages="volunteerForm.lastname.$error" ng-if="volunteerForm.lastname.$touched" ng-hide="volunteerForm.lastname.$valid">
-		<p ng-message="required">Please enter a last name</p>
-		</div>
-	</div>
-	<!--contact info-->
-	<label class="control-label" for="firstname">Contact</label>
-	<div class="form-inline form-group-lg" ng-class="{ 'has-error': volunteerForm.email.$touched && volunteerForm.email.$invalid }">
-		<!--email-->
-		<label class="control-label sr-only" for="email">Email</label>
-		<div class="input-group">
-			<div class="input-group-addon">
-				<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+			<div class="alert alert-danger" role="alert" ng-messages="volunteerForm.firstname.$error" ng-if="volunteerForm.firstname.$touched" ng-hide="volunteerForm.firstname.$valid">
+				<p ng-message="required">Please enter a first name</p>
 			</div>
-			<input type="email" class="form-control" id="email" name="email" placeholder="Email" ng-model="volData.email" ng-required="true" />
-		</div>
-		<div class="alert alert-danger" role="alert" ng-messages="volunteerForm.email.$error" ng-if="volunteerForm.email.$touched" ng-hide="volunteerForm.email.$valid">
-			<p ng-message="email">Email is invalid.</p>
-			<p ng-message="required">Please enter an email.</p>
-		</div>
-		<!--phone-->
-		<label class="control-label sr-only " for="phone">Phone</label>
-		<div class="input-group">
-			<div class="input-group-addon">
-				<span class="glyphicon glyphicon-phone" aria-hidden="true"></span>
+			<!--last name-->
+			<label class="control-label sr-only" for="lastname">Last Name</label>
+			<input type="text" class="form-control" id="lastname" name="lastname" placeholder="Last Name" ng-model="volData.lastname" ng-required="true">
+			<div class="alert alert-danger" role="alert" ng-messages="volunteerForm.lastname.$error" ng-if="volunteerForm.lastname.$touched" ng-hide="volunteerForm.lastname.$valid">
+			<p ng-message="required">Please enter a last name</p>
 			</div>
-			<input type="text" class="form-control" id="phone" name="phone" placeholder="Phone" ng-model="volData.phone" ng-required="true" />
 		</div>
-		<div class="alert alert-danger" role="alert" ng-messages="volunteerForm.phone.$error" ng-if="volunteerForm.phone.$touched" ng-hide="volunteerForm.phone.$valid">
-			<p ng-message="required">Please enter a phone number.</p>
+		<!--contact info-->
+		<label class="control-label" for="firstname">Contact</label>
+		<div class="form-inline form-group-lg" ng-class="{ 'has-error': volunteerForm.email.$touched && volunteerForm.email.$invalid }">
+			<!--email-->
+			<label class="control-label sr-only" for="email">Email</label>
+			<div class="input-group">
+				<div class="input-group-addon">
+					<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+				</div>
+				<input type="email" class="form-control" id="email" name="email" placeholder="Email" ng-model="volData.email" ng-required="true" />
+			</div>
+			<div class="alert alert-danger" role="alert" ng-messages="volunteerForm.email.$error" ng-if="volunteerForm.email.$touched" ng-hide="volunteerForm.email.$valid">
+				<p ng-message="email">Email is invalid.</p>
+				<p ng-message="required">Please enter an email.</p>
+			</div>
+			<!--phone-->
+			<label class="control-label sr-only " for="phone">Phone</label>
+			<div class="input-group">
+				<div class="input-group-addon">
+					<span class="glyphicon glyphicon-phone" aria-hidden="true"></span>
+				</div>
+				<input type="text" class="form-control" id="phone" name="phone" placeholder="Phone" ng-model="volData.phone" ng-required="true" />
+			</div>
+			<div class="alert alert-danger" role="alert" ng-messages="volunteerForm.phone.$error" ng-if="volunteerForm.phone.$touched" ng-hide="volunteerForm.phone.$valid">
+				<p ng-message="required">Please enter a phone number.</p>
+			</div>
+			<hr />
+			<h4>Is this person an administrator?</h4>
+			<div class="checkbox ">
+				<label>
+					<input type="checkbox" name="isAdmin" id="isAdmin" value="">
+					Check to create new administrator.
+				</label>
+			</div>
 		</div>
 		<hr />
-		<h4>Is this person an administrator?</h4>
-		<div class="checkbox ">
-			<label>
-				<input type="checkbox" name="isAdmin" id="isAdmin" value="">
-				Check to create new administrator.
-			</label>
-		</div>
-	</div>
-	<hr />
-	<button type="submit" class="btn btn-lg btn-info" ng-click="ok();" ng-disabled="volunteerForm.$invalid"><i class="fa fa-check" aria-hidden="true"></i>Submit</button>
-	<button type="reset" class="btn btn-lg btn-warning" ng-click="cancel();"><i class="fa fa-ban" aria-hidden="true"></i> Cancel</button>
-</form>
+		<button type="submit" class="btn btn-lg btn-info" ng-click="ok();" ng-disabled="volunteerForm.$invalid"><i class="fa fa-check" aria-hidden="true"></i>Submit</button>
+		<button type="reset" class="btn btn-lg btn-warning" ng-click="cancel();"><i class="fa fa-ban" aria-hidden="true"></i> Cancel</button>
+	</form>
+</div>
