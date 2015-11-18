11b07c8989ad0bba6a31a0058cb5b077eee828a8
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-18 10:25:01 -0700

    added utilities page https://app.asana.com/0/60993780339150/63421877783062

---
+++ public_html/php/template/utilities.php
@@ -0,0 +1,34 @@
+<!--current utilities for each page-->
+	<meta charset="utf-8"/>
+	<meta http-equiv="x-ua-compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+	<!-- CSS stylesheets -->
+	<!--latest compiled and minified bootstrap css files-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+	<link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+	<!--optional theme-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+	<!--minified font awesome css-->
+	<link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+	<!-- CUSTOM stylesheets -->
+	<link type="text/css" rel="stylesheet" href="../../css/custom-style.css"/>
+
+	<!--jQuery for Bootstrap's .js plugins-->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<!--latest compiled and minified bootstrap javascript-->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
+	<!--CDN derived Angular.js -->
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular-messages.min.js"></script>
+	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.14.3/ui-bootstrap-tpls.min.js"></script>
+
+	<!-- CUSTOM js-->
+	<script type="text/javascript" src="../../js/angular-password.min.js"></script>
+	<script type="text/javascript" src="../../js/breadbasket.js"></script>
+	<script type="text/javascript" src="../../js/controllers/tabs.js"></script>
+	<script type="text/javascript" src="../../js/controllers/signup-modal.js"></script>
+	<script type="text/javascript" src="../../js/controllers/signup-controller.js"></script>
+
