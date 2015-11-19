104e646be5183bf52f34969397f244c8bc9b9285
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-19 16:40:31 -0700

    added head utils php https://app.asana.com/0/60993780339150/63421877783062

--- public_html/php/template/utilities.php
+++ public_html/php/template/utilities.php
@@ -1,13 +1,24 @@
-<!--current utilities for each page-->
-
 <?php
+/**
+ * Get the relative path.
+ * @see https://raw.githubusercontent.com/kingscreations/farm-to-you/master/php/lib/_header.php FarmToYou Header
+ **/
+
+// include the appropriate number of dirname() functions
+// on line 8 to correctly resolve your directory's path
+require_once(dirname(dirname(__DIR__)) . "/root-path.php");
+$CURRENT_DEPTH = substr_count($CURRENT_DIR, "/");
+$ROOT_DEPTH = substr_count($ROOT_PATH, "/");
+$DEPTH_DIFFERENCE = $CURRENT_DEPTH - $ROOT_DEPTH;
+$PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
+
 //start the session and create a XSRF token
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 verifyXsrf();
 ?>
-
+<!--current utilities for each page-->
 <!DOCTYPE html>
 <html lang="en" ng-app="BreadBasket">
 	<head>
