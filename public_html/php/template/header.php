4860a55d680b549bd75c266c8bb8e73cee7980f7
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-19 16:09:20 -0700

    added header page that includes nav https://app.asana.com/0/60993780339150/63421877783062

---
+++ public_html/php/template/header.php
@@ -0,0 +1,30 @@
+
+<div class="container-fluid">
+	<!--begin navbar-->
+	<nav class="nav navbar-default">
+		<!--logo and mobile toggle button get grouped together-->
+		<div class="navbar-header">
+			<button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#my-navbar" aria-expanded="false">
+				<span class="sr-only">Main Menu</span>
+				<span class="glyphicon glyphicon-th-large"></span>
+			</button>
+			<a href="#" class="navbar-brand">
+				<span class="glyphicon glyphicon-grain"></span>
+				Bread Basket
+			</a>
+		</div>
+
+		<!--nav links are grouped together here-->
+		<div class="collapse navbar-collapse navbar-right" id="my-navbar">
+			<ul class="nav navbar-nav">
+				<li><a href="#">Login</a> </li>
+				<li><a href="#">Sign Up</a> </li>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">My Organization</a></li>
+				<li><a href="#">Listings</a></li>
+				<li><a href="#">Volunteers</a> </li>
+				<li><a href="#">Help</a></li>
+			</ul>
+		</div>
+	</nav>
+</div>
