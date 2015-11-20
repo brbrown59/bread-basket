8e49721dd6624c6b227955bc1a36f43f7774517e
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-20 11:40:07 -0700

    set up test organization and volunteer entries https://app.asana.com/0/60990646650926/65845316003349

--- test/org-api-test.php
+++ test/org-api-test.php
@@ -9,6 +9,21 @@ class OrganizationApiTest {
 	protected $guzzle = null;
 
 	public final function setUp() {
+		//create a new organization for the test volunteers to belong
+		$organization = new Organization(null, "123 Easy Street", '', "Albuquerque", "Feeding people since 1987", "9 - 5", "Feed the People", "505-765-4321", "NM", "R", "87801");
+
+		//create a new volunteer to use as an admin for the tests
+		$salt = bin2hex(openssl_random_pseudo_bytes(32));
+		$hash =  hash_pbkdf2("sha512", "password4321", $salt, 262144, 128);
+		$admin = new Volunteer(null, $organization->getOrgId(), "fakeemail@fake.com", null, "John", $hash, true, "Doe", "505-123-4567", $salt);
+
+		//create a non-admin volunteer for the tests
+		$salt = bin2hex(openssl_random_pseudo_bytes(32));
+		$hash =  hash_pbkdf2("sha512", "password4321", $salt, 262144, 128);
+		$volunteer = new Volunteer(null, $organization->getOrgId(), "notanemail@fake.com", null, "Jane", $hash, true, "Doe", "505-555-5555", $salt);
+
+		//sign-in the volunteers, or just make dummy sessions?
+
 		// visit ourselves to get the cookie
 		$this->guzzle = $client = new \GuzzleHttp\Client(["cookies" => true]);
 
@@ -18,7 +33,10 @@ class OrganizationApiTest {
 		var_dump($cookies->getCookieByName("XSRF-TOKEN"));
 
 	}
+
+	//test getting a valid entry and making sure it
 	public function testValidGet() {
+		//test getting by parameter x
 
 	}
 	public function testInvalidGet() {
@@ -45,6 +63,3 @@ class OrganizationApiTest {
 
 
 }
-
-$test = new OrganizationApiTest();
-$test->setUp();
