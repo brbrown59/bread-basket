192bff6327fce95965d9d5a6364453ea647f715b
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-20 12:14:43 -0700

    added links to correct pages in nav  https://app.asana.com/0/60993780339150/63421877783062

--- public_html/php/template/header.php
+++ public_html/php/template/header.php
@@ -17,10 +17,10 @@
 		<!--nav links are grouped together here-->
 		<div class="collapse navbar-collapse navbar-right" id="my-navbar">
 			<ul class="nav navbar-nav">
-				<li><a href="#">Home</a></li>
-				<li><a href="#">Organization</a></li>
-				<li><a href="#">Listings</a></li>
-				<li><a href="#">Volunteers</a> </li>
+				<li><a href="login-landing-page.php">Home</a></li>
+				<li><a href="org-profile-view.php">Organization</a></li>
+				<li><a href="listing-view.php">Listings</a></li>
+				<li><a href="volunteer-list-view.php">Volunteers</a> </li>
 				<li><a href="#">Help</a></li>
 			</ul>
 		</div>
