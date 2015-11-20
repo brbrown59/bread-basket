db9492c75c4acb10443e27d1f95ab1d864e0daa8
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-20 11:17:09 -0700

    added google analytics  https://app.asana.com/0/60993780339150/63421877783062

--- public_html/php/template/utilities.php
+++ public_html/php/template/utilities.php
@@ -63,7 +63,19 @@ setXsrfCookie();
 		<script type="text/javascript" src="../../js/controllers/newvolunteer-controller.js"></script>
 		<script type="text/javascript" src="../../js/controllers/newlisting-controller.js"></script>
 
+		<!--Page Title-->
 		<title><?php echo $PAGE_TITLE; ?></title>
+
+		<!-- Google Analytics -->
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+						(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new
+							Date();a=s.createElement(o),
+					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-64861502-1', 'auto');
+			ga('send', 'pageview');
+		</script>
 	</head>
 		<body>
 
