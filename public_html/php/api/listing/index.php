9d7ca1955ba185abd48543410b3ce742e9331364
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-23 16:48:44 -0700

    put the session determination in fron of the http method and got an "unreachable statement" error  https://app.asana.com/0/60993780339150/65844768948496

--- public_html/php/api/listing/index.php
+++ public_html/php/api/listing/index.php
@@ -25,6 +25,13 @@ try {
 	$pusherConfig = json_decode($config["pusher"]);
 	$pusher = new Pusher($pusherConfig->key, $pusherConfig->secret, $pusherConfig->id, ["encrypted" => true]);
 
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/breadbasket.ini");
+
+	//if the volunteer session is empty, the user is not logged in, throw an exception
+	if(empty($_SESSION["volunteer"]) === true) {
+		throw(new RuntimeException("Please log-in or sign up", 401));
+
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
@@ -33,13 +40,11 @@ try {
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
+	//sanitize and trim the other fields
+	$memo= filter_input(INPUT_GET, "memo", FILTER_SANITIZE_STRING);
+	$cost = filter_input(INPUT_GET, "cost", FILTER_VALIDATE_FLOAT);
+	$state = filter_input(INPUT_GET, "type", FILTER_VALIDATE_INT);
 
-	//grab the mySQL connection
-	$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/breadbasket.ini");
-
-	//if the volunteer session is empty, the user is not logged in, throw an exception
-	if(empty($_SESSION["volunteer"]) === true) {
-		throw(new RuntimeException("Please log-in or sign up", 401));
 	}
 
 	//handle all RESTful calls to Listing
