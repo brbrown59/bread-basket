a7b628ba33bad6b19ef82418e0388bf4db72c399
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-23 16:18:37 -0700

    started the listing API  https://app.asana.com/0/60993780339150/65844768948496

---
+++ public_html/php/api/listing/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once dirname(dirname(__DIR__)) . "/classes/autoloader.php";
+require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+/**
+ * controller/api for the listing class
+ *
+ * @author Kimberly Keller keller.kimberly@gmail.com>
+ */
+
+//verify the xsrf challenge
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//create the Pusher connection
+	$config = readConfig("/etc/apache2/capstone-mysql/breadbasket.ini");
+	$pusherConfig = json_decode($config["pusher"]);
+	$pusher = new Pusher($pusherConfig->key, $pusherConfig->secret, $pusherConfig->id ["enrypted" => true]);
+}
