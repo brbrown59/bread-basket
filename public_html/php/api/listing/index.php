a9ec110776eb993f5168e650e813fecc9c03ab93
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-24 09:21:46 -0700

    started with put and post https://app.asana.com/0/60993780339150/65844768948496

--- public_html/php/api/listing/index.php
+++ public_html/php/api/listing/index.php
@@ -19,61 +19,93 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
-try {
-	//create the Pusher connection
-	$config = readConfig("/etc/apache2/capstone-mysql/breadbasket.ini");
-	$pusherConfig = json_decode($config["pusher"]);
-	$pusher = new Pusher($pusherConfig->key, $pusherConfig->secret, $pusherConfig->id, ["encrypted" => true]);
+	try {
+		//create the Pusher connection
+		$config = readConfig("/etc/apache2/capstone-mysql/breadbasket.ini");
+		$pusherConfig = json_decode($config["pusher"]);
+		$pusher = new Pusher($pusherConfig->key, $pusherConfig->secret, $pusherConfig->id, ["encrypted" => true]);
 
-	//determine which HTTP method was used
-	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+		//determine which HTTP method was used
+		$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
-	//if the volunteer session is empty, the user is not logged in, throw an exception
-	if(empty($_SESSION["volunteer"]) === true) {
-		throw(new RuntimeException("Please log-in or sign up", 401));
-	}
+		//if the volunteer session is empty, the user is not logged in, throw an exception
+		if(empty($_SESSION["volunteer"]) === true) {
+			throw(new RuntimeException("Please log-in or sign up", 401));
+		}
 
-	//sanitize the id
-	$id = filter_input(INPUT_GET, "id" FILTER_VALIDATE_INT);
-	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
-		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
-	}
-	//sanitize and trim the other fields
-	$memo= filter_input(INPUT_GET, "memo", FILTER_SANITIZE_STRING);
-	$cost = filter_input(INPUT_GET, "cost", FILTER_VALIDATE_FLOAT);
-	$state = filter_input(INPUT_GET, "type", FILTER_VALIDATE_INT);
+		//sanitize the id
+		$id = filter_input(INPUT_GET, "id" FILTER_VALIDATE_INT);
+		if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+			throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+		}
+		//sanitize and trim the other fields
+		$memo= filter_input(INPUT_GET, "memo", FILTER_SANITIZE_STRING);
+		$cost = filter_input(INPUT_GET, "cost", FILTER_VALIDATE_FLOAT);
+		$state = filter_input(INPUT_GET, "type", FILTER_VALIDATE_INT);
 
-	//grab the mySQL connection
-	$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/breadbasket.ini");
+		//grab the mySQL connection
+		$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/breadbasket.ini");
 
-	//handle all RESTful calls to listing
-	//get some or all Listings
-	if($method === "GET") {
-		//set an XSRF cookie on get requests
-		setXsrfCookie("/");
-		if(empty($id) === false) {
-			$reply->data = Listing::getListingByListingId($pdo, $id);
-		} elseif(empty($orgId)) {
-			$reply->data = Listing::getListingByOrgId($pdo, $orgId);
-		} elseif(empty($listingPostTime)) {
-			$reply->data = Listing::getListingByListingPostTime($pdo, $listingPostTime);
-		} elseif(empty($listing)) {
-			$reply->data = Listing::getListingByParentId($pdo, $listingParentId);
-		} elseif(empty($listingTypeId)) {
-			$reply->data = Listing::getListingByTypeId($pdo, $listingTypeId);
-		} else {
-			$reply->data = Listing::getAllListings($pdo)->toArray();
+		//handle all RESTful calls to listing
+		//get some or all Listings
+		if($method === "GET") {
+			//set an XSRF cookie on get requests
+			setXsrfCookie("/");
+			if(empty($id) === false) {
+				$reply->data = Listing::getListingByListingId($pdo, $id);
+			} elseif(empty($orgId) === false) {
+				$reply->data = Listing::getListingByOrgId($pdo, $orgId);
+			} elseif(empty($postTime) === false) {
+				$reply->data = Listing::getListingByListingPostTime($pdo, $postTime)->toArray();
+			} elseif(empty($parentId) === false) {
+				$reply->data = Listing::getListingByParentId($pdo, $parentId)->toArray();
+			} elseif(empty($typeId) === false) {
+				$reply->data = Listing::getListingByTypeId($pdo, $typeId)->toArray();
+			} else {
+				$reply->data = Listing::getAllListings($pdo)->toArray();
+			}
+		}
+		//verify admin and verify object not empty
+
+		//if the session belongs to an admin, allow post, put, and delete methods
+		if(empty($_SESSION["volunteer"]) === false && $_SESSION["volunteer"]->getVolIsAdmin() === true) {
+
+			if($method === "PUT" || $method === "POST") {
+
+				verifyXsrf();
+				$requestContent = file_get_contents("php://input");
+				$requestObject = json_decode($requestContent);
+
+				//make sure all fields are present, in order to prevent database issues
+				if(empty($requestObject->orgId) === true) {
+					throw(new InvalidArgumentException ("organization id cannot be empty", 405));
+				}
+				if(empty($requestObject->listingClaimedBy) === true) {
+					$requestObject->listingClaimedBy = null;
+				}
+				if(empty($requestObject->listingClosed) === true) {
+					$requestObject->listingClosed = null;
+				}
+				if(empty($requestObject->listingCost) === true) {
+					$requestObject->listingCost = null;
+				}
+				if(empty($requestObject->listingMemo) === true) {
+					throw(new InvalidArgumentException("listing memo cannot be empty", 405));
+				}
+				if(empty($requestObject->listingParentId) === true) {
+					$requestObject->listingParentId = null;
+				}
+				if(empty($requestObject->listingPostTime) === true) {
+					throw(new InvalidArgumentException("listing post time cannot be empty", 405));
+				}
+				if(empty($requestObject->listingTypeId) === true) {
+					throw(new InvalidArgumentException("listing type cannot be empty", 405));
+				}
+
+			}
+
+
 		}
 
 
-
 	}
-
-
-
-
-	}
-
-	//handle all RESTful calls to Listing
-	//get some or all listings
-}
