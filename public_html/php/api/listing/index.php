c328109ee13be5f5998101f6ff3b7f27d9bf9ad6
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-24 08:54:41 -0700

    started restful calls https://app.asana.com/0/60993780339150/65844768948496

--- public_html/php/api/listing/index.php
+++ public_html/php/api/listing/index.php
@@ -25,15 +25,13 @@ try {
 	$pusherConfig = json_decode($config["pusher"]);
 	$pusher = new Pusher($pusherConfig->key, $pusherConfig->secret, $pusherConfig->id, ["encrypted" => true]);
 
-	//grab the mySQL connection
-	$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/breadbasket.ini");
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
 	//if the volunteer session is empty, the user is not logged in, throw an exception
 	if(empty($_SESSION["volunteer"]) === true) {
 		throw(new RuntimeException("Please log-in or sign up", 401));
-
-	//determine which HTTP method was used
-	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	}
 
 	//sanitize the id
 	$id = filter_input(INPUT_GET, "id" FILTER_VALIDATE_INT);
@@ -44,6 +42,35 @@ try {
 	$memo= filter_input(INPUT_GET, "memo", FILTER_SANITIZE_STRING);
 	$cost = filter_input(INPUT_GET, "cost", FILTER_VALIDATE_FLOAT);
 	$state = filter_input(INPUT_GET, "type", FILTER_VALIDATE_INT);
+
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/breadbasket.ini");
+
+	//handle all RESTful calls to listing
+	//get some or all Listings
+	if($method === "GET") {
+		//set an XSRF cookie on get requests
+		setXsrfCookie("/");
+		if(empty($id) === false) {
+			$reply->data = Listing::getListingByListingId($pdo, $id);
+		} elseif(empty($orgId)) {
+			$reply->data = Listing::getListingByOrgId($pdo, $orgId);
+		} elseif(empty($listingPostTime)) {
+			$reply->data = Listing::getListingByListingPostTime($pdo, $listingPostTime);
+		} elseif(empty($listing)) {
+			$reply->data = Listing::getListingByParentId($pdo, $listingParentId);
+		} elseif(empty($listingTypeId)) {
+			$reply->data = Listing::getListingByTypeId($pdo, $listingTypeId);
+		} else {
+			$reply->data = Listing::getAllListings($pdo)->toArray();
+		}
+
+
+
+	}
+
+
+
 
 	}
 
