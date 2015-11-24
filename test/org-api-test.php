f1fd597db0cdd6b9398cd0f666148b837e8edd36
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-23 19:26:04 -0700

    modified code based on inventory management example: getting ACTUAL status code changes!!!! https://app.asana.com/0/60990646650926/65845316003349

--- test/org-api-test.php
+++ test/org-api-test.php
@@ -104,13 +104,38 @@ class OrganizationApiTest extends BreadBasketTest {
 		$hash =  hash_pbkdf2("sha512", "password4321", $salt, 262144, 128);
 		$this->volunteer = new Volunteer(null, $organization->getOrgId(), "notanemail@fake.com", null, "Jane", $hash, false, "Doe", "505-555-5555", $salt);
 
-		// visit ourselves to get the XSRF-TOKEN cookie
-		$this->guzzle = $client = new \GuzzleHttp\Client(["cookies" => true]);
-		$output = $this->guzzle->request("GET", 'https://bootcamp-coders.cnm.edu/'); //this does not assign an XSRF token if I visit my api
+		//create the guzzle client
+		$this->guzzle = new \GuzzleHttp\Client(["cookies" => true]);
 
+	}
+
+
+	//test deleting a valid entry
+	public function testValidDelete() {
+		//create a new organization, and insert into the database
+		$organization = new Organization(null, $this->VALID_ADDRESS1, $this->VALID_ADDRESS2, $this->VALID_CITY, $this->VALID_DESCRIPTION,
+				$this->VALID_HOURS, $this->VALID_NAME, $this->VALID_PHONE, $this->VALID_STATE, $this->VALID_TYPE, $this->VALID_ZIP);
+		$organization->insert($this->getPDO());
+
+		//visit ourselves to get the token
+		$this->guzzle->get('https://bootcamp-coders.cnm.edu/'); //this does not assign an XSRF token if I visit my api
 		$cookies = $this->guzzle->getConfig()["cookies"];
 		$this->token = $cookies->getCookieByName("XSRF-TOKEN");
 
+		// grab the data from guzzle and enforce that the status codes are correct
+		//this directory path's not going to work on anyone else's deployment...
+		//also, need to worry about the sessions
+		$response = $this->guzzle->delete('https://bootcamp-coders.cnm.edu/~bbrown52/bread-basket/public_html/php/api/organization' . $organization->getOrgId(),
+				['headers' => ['X-XSRF-TOKEN' => $this->token]
+		]);
+		$this->assertSame($response->getStatusCode(), 200);
+		$body = $response->getBody();
+		$retrievedOrg = json_decode($body);
+		$this->assertSame(200, $retrievedOrg->status);
+
+	}
+	public function testInvalidDelete() {
+		//test to make sure can't delete organization that doesn't exist
 	}
 
 
@@ -122,27 +147,24 @@ class OrganizationApiTest extends BreadBasketTest {
 		$organization = new Organization(null, $this->VALID_ADDRESS1, $this->VALID_ADDRESS2, $this->VALID_CITY, $this->VALID_DESCRIPTION,
 				$this->VALID_HOURS, $this->VALID_NAME, $this->VALID_PHONE, $this->VALID_STATE, $this->VALID_TYPE, $this->VALID_ZIP);
 
-		//encode the newly created object into json, in order to send it via request
-		//might need to get rid of this and let guzzle do the encoding
-		$organization = json_encode($organization);
+		//visit ourselves to get the token
+		$this->guzzle->get('https://bootcamp-coders.cnm.edu/'); //this does not assign an XSRF token if I visit my api
+		$cookies = $this->guzzle->getConfig()["cookies"];
+		$this->token = $cookies->getCookieByName("XSRF-TOKEN");
 
 		//send organization info to api in a put method, also make sure the cookie is set
-		//TODO: figure out a. how to send the xsrf token in the header (might need an entire jar, with c. included) b. how to send the body of a request as json c. does session need to be set in header?
-		$insert = $this->guzzle->request("POST", 'https://bootcamp-coders.cnm.edu/~bbrown52/bread-basket/public_html/php/api/organization', [
-				'json' => $organization, //does this work?????
-				'cookies' => $this->token //doesn't work; has to be a "CookieJarInterface"
+		//TODO: does session info need to be set in header?
+		$response = $this->guzzle->post('https://bootcamp-coders.cnm.edu/~bbrown52/bread-basket/public_html/php/api/organization', [
+				'json' => $organization,
+				'headers' => ['X-XSRF-TOKEN' => $this->token] //session here?
 		]);
+		//make sure the status codes match
+		//...do we not check to make sure the content matches expectations
+		$this->assertSame($response->getStatusCode(), 200);
+		$body = $response->getBody();
+		$retrievedOrg = json_decode($body);
+		$this->assertSame(200, $retrievedOrg->status);
 
-		//get info from the api, and confirm it matches the given values
-		//don't have the id, so use the unique name field
-		$output = $this->guzzle->request("GET", 'https://bootcamp-coders.cnm.edu/~bbrown52/bread-basket/public_html/php/api/organization', [
-				'query' => ['name' => $this->VALID_NAME]
-		]);
-
-		$body = $output->getBody(); //get something useful from this; turn into an organization object?????
-		//var_dump($body);
-
-		//actual comparisons here
 	}
 	public function testInvalidPost() {
 		//test to make sure non-admin can't put (same code would execute for put and delete, so test it here and not there)
@@ -157,12 +179,7 @@ class OrganizationApiTest extends BreadBasketTest {
 		//test to make sure can't put to an organization that doesn't exist
 		//question: what about the massive if block to check for empty fields?  can't do ALL of them
 	}
-	public function testValidDelete() {
 
-	}
-	public function testInvalidDelete() {
-			//test to make sure can't delete organization that doesn't exist
-	}
 
 	public function testValidGet() {
 		//test getting by parameter x
