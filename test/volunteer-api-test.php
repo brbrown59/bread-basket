512df35da92aecd684b7162220fb8663fb69de35
Kimberly Keller <kimberly@gravitaspublications.com>
2015-11-25 10:00:48 -0700

    added delete method to test https://app.asana.com/0/60993780339150/65845306044889

--- test/volunteer-api-test.php
+++ test/volunteer-api-test.php
@@ -55,11 +55,6 @@ class VolunteerApiTest extends BreadBasketTest {
 	 */
 	protected $VALID_HASH;
 	/**
-	 * valid password for volunteer
-	 * @var string $password
-	 */
-	protected $VALID_PASSWORD = "idonatefood";
-	/**
 	 * Guzzle client used to perform the tests
 	 * @var GuzzleHttp/Client $guzzle
 	 */
@@ -82,8 +77,58 @@ class VolunteerApiTest extends BreadBasketTest {
 		//run default set-up method
 		parent::setUp();
 
-		//create an organization for the volunteers to be a part of.
+		//create salt and hash
+		$this->VALID_HASH = hash_pbkdf2("sha512", "password1234", $this->VALID_salt, 262144, 128);
+		$this->VALID_SALT = bin2hex(openssl_random_pseudo_bytes(32));
+
+		//create an organization for the volunteers to be a part of
+		$organization = new Organization(null, "123 Easy Street", '', "Albuquerque", "Feeding people since 1987", "9 - 5", "Food for Hungry People", "505-765-4321", "NM", "R", "87801");
+		$organization->insert($this->getPDO());
+
+
+		//create the guzzle client
+		$this->guzzle = new \GuzzleHttp\Client(["cookies" => true]);
+
+		//visit ourselves to get the xsrf-token
+		$this->guzzle->get('https://bootcamp-coders.cnm.edu/~bbrown52/bread-basket/public_html/php/api/organization');
+		$cookies = $this->guzzle->getConfig()["cookies"];
+		$this->token = $cookies->getCookieByName("XSRF-TOKEN")->getValue();
+
+
+		//send a request to the sign-in method
+
+		$adminLogin = new stdClass();
+		$adminLogin->email = "lastweektonight@joliver.com";
+		$adminLogin->password = "idonatefood";
+		$login = $this->guzzle->post('https://bootcamp-coders.cnm.edu/~bbrown52/bread-basket/public_html/php/controllers/sign-in-controller.php', [
+				'json' => $adminLogin,
+				'headers' => ['X-XSRF-TOKEN' => $this->token]
+		]);
 
 	}
+
+	//test deleting a valid entry
+	public function testValidDelete() {
+
+		//create a new organization, and insert into the database
+		$volunteer = new Volunteer(null, $this->organization->getOrgId(), $this->VALID_EMAIL, $this->VALID_EMAIL_ACTIVATION, $this->VALID_FIRST_NAME, $this->VALID_HASH, $this->VALID_ADMIN, $this->VALID_LAST_NAME, $this->VALID_PHONE, $this->VALID_SALT);
+		$volunteer->insert($this->getPDO());
+
+		// grab the data from guzzle and enforce that the status codes are correct
+		$response = $this->guzzle->delete('https://bootcamp-coders.cnm.edu/~bbrown52/bread-basket/public_html/php/api/organization/' . $organization->getOrgId(),
+				['headers' => ['X-XSRF-TOKEN' => $this->token]
+				]);
+		$this->assertSame($response->getStatusCode(), 200);
+		$body = $response->getBody();
+		$retrievedVol = json_decode($body);
+		$this->assertSame(200, $retrievedVol->status);
+
+		//try retrieving entry from database and ensuring it was deleted
+		$deletedVol = Volunteer::getVolunteerByVolId($this->getPDO(), $volunteer->getOrgId());
+		$this->assertNull($deletedVol);
+
+	}
+
+
 
 }
