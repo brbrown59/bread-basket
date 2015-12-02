aff8a9dc5a973bfada86d6dfd96445508fdc772d
Carlos Beraun <cberaun2@cnm.edu>
2015-12-02 08:08:37 -0700

    added messageText to argument (message/index.php)---->https://app.asana.com/0/60990639151493/65845291756892

--- public_html/php/api/message/index.php
+++ public_html/php/api/message/index.php
@@ -61,6 +61,8 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 					$reply->data = Message::getMessageByListingId($pdo, $listingId)->toArray();
 				} else if(empty ($orgId) === false) {
 					$reply->data = Message::getMessageByOrgId($pdo, $orgId)->toArray();
+				} else if(empty ($orgId) === false) {
+					$reply->data = Message::getMessageBymessageText($pdo, $messageId)->toArray();
 				} else{
 					$reply->data = Message::getAllMessages($pdo)->toArray();
 				}
