c7565d9c1c9d8861f644a43e981516654cda13ce
Bradley Brown <tall.white.ninja@gmail.com>
2015-12-03 13:42:53 -0700

    more progress on organization controller https://app.asana.com/0/inbox/60990646650925/70724582720491/70724582720494

--- public_html/js/controllers/organization-controller.js
+++ public_html/js/controllers/organization-controller.js
@@ -2,7 +2,18 @@ app.controller("OrganizationController", ["$scope", "$uibModal", "OrganizationSe
 	//add as needed
 	$scope.organizations = [];
 	$scope.alerts = [];
-	$scope.isEditing = false;//look into this one
+	$scope.isEditing = false;
+
+	//set the isEditing value to true
+	//here in case we want to add an editing "well", may be omitted/altered later
+	$scope.setEditedOrganization = function() {
+		$scope.isEditing = true;
+	};
+
+	//cancels editing
+	$scope.cancelEditing = function() {
+		$scope.isEditing = false;
+	}
 
 	//get orgs from api
 	//come back for the other gets
@@ -16,6 +27,19 @@ app.controller("OrganizationController", ["$scope", "$uibModal", "OrganizationSe
 						$scope.alerts[0] = {type: "danger", msg: result.data.message};
 					}
 				});
+	};
+
+	$scope.getOrganizationById = function(orgId, validated) {
+		if(validated === true) {
+			OrganizationService.fetch(orgId)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.organizations = result.data.data;
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+		}
 	};
 
 	//create new organization
@@ -34,7 +58,7 @@ app.controller("OrganizationController", ["$scope", "$uibModal", "OrganizationSe
 
 	//update the organization
 	$scope.updateOrganization = function(organization, validated) {
-		if(validated === true && $scope.isEditing === true) {
+		if(validated === true && $scope.isEditing === true) { //that scope.isEditing may change
 			OrganizationService.update(organization.orgId, organization)
 					.then(function(result) {
 						if(result.data.status === 200) {
@@ -69,3 +93,13 @@ app.controller("OrganizationController", ["$scope", "$uibModal", "OrganizationSe
 		});
 	};
 }]);
+
+//modal instance controller for deletion prompt
+var ModalInstanceCtrl = function($scope, $uibModalInstance) {
+	$scope.yes = function() {
+		$uibModalInstance.close();
+	};
+	$scope.no = function() {
+		$uibModalInstance.dismiss('cancel');
+	};
+};
