f35f5cc8fcfcee35ef42cc0ad71af645deca5b6e
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-12-03 09:17:28 -0700

    js Sign In https://app.asana.com/0/60993780339172/70723740899547

---
+++ public_html/js/services/sign-in-service.js
@@ -0,0 +1,19 @@
+/**
+ * A service for sign-in
+ *
+ * @author Tamra Fenstermaker <fenstermaker505@gmail.com>
+ * contributing code from ng-abq @author Dylan McDonald
+ */
+
+//"signinService refers to what's in the signin-controller. Todo add Sign in Service to js SignIn Controller TODO is this the correct endpoint?
+app.service("SigninService", function($http){
+	this.SIGNUP_ENDPOINT = "/auth/signin";
+
+	this.signin = function(signinData) { //signinData from the signin-controller and signin-modal
+		return($http.post(this.SIGNUP_ENDPOINT, signinData)
+				.then(function(reply) {
+					return(reply.data);
+				}));
+	};
+});
+
