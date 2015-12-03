ce35b7b410c0a556b143bf90253fc5e3e288c29a
Bradley Brown <tall.white.ninja@gmail.com>
2015-12-03 09:54:24 -0700

    service rough draft done https://app.asana.com/0/inbox/60990646650925/70724582720491/70724582720494

--- public_html/js/services/organization-service.js
+++ public_html/js/services/organization-service.js
@@ -1,33 +1,14 @@
 app.constant("ORGANIZATION_ENDPOINT", "php/api/organization/");//check the end point
 app.service("OrganizationService", function($http, ORGANIZATION_ENDPOINT) {
+	//internal function for holding the url
 	function getUrl() {
 		return(ORGANIZATION_ENDPOINT);
 	}
 
+	//internal function for tacking the id on to the url
 	function getUrlForId(orgId) {
 		return(getUrl() + orgId);
 	}
-
-	function getUrlForCity(orgCity) {
-		return(getUrl() + "?city=" + orgCity);
-	}
-
-	function getUrlForName(orgName) {
-		return(getUrl() + "?name=" + orgName);
-	}
-
-	function getUrlForState(orgState) {
-		return(getUrl() + "?state=" + orgState);
-	}
-
-	function getUrlForType(orgType) {
-		return(getUrl() + "?type=" + orgType);
-	}
-
-	function getUrlForZip(orgZip) {
-		return(getUrl() + "?zip=" + orgZip);
-	}
-
 
 	//getting all
 	this.all = function() {
@@ -38,19 +19,38 @@ app.service("OrganizationService", function($http, ORGANIZATION_ENDPOINT) {
 	this.fetch = function(orgId) {
 		return($http.get(getUrlForId(orgId)));
 	};
-	//need to write the other gets, probably with more "getUrlForX" functions
-	//also need to figure out what to use besides .fetch
-	//might need to unwrite the thing above to handle multiple use cases
-	//org IDs are probably going to be derived from the volunteer table 90% of the time
+	//get by city
+	this.fetchcity = function(orgCity) {
+		return($http.get(getUrl() + "?city=" + orgCity));
+	};
+	//get by name
+	this.fetchname = function(orgName) {
+		return($http.get(getUrl() + "?name=" + orgName));
+	};
+	//get by state
+	this.fetchstate = function(orgState) {
+		return($http.get(getUrl() + "?state=" + orgState));
+	};
+	//get by type
+	this.fetchtype = function(orgType) {
+		return($http.get(getUrl() + "?type=" + orgType));
+	};
+	//get by zip
+	this.fetchzip = function(orgZip) {
+		return($http.get(getUrl() + "?zip=" + orgZip));
+	};
 
-	this.create = function(organization) { //should be taking in an entire chunk of JSON???
+	//post
+	this.create = function(organization) {
 		return($http.post(getUrl(), organization));
 	};
 
+	//put
 	this.update = function(orgId, organization) {
 		return($http.put(getUrlForId(orgId), organization));
 	};
 
+	//delete
 	this.destroy = function(orgId) {
 		return($http.delete(getUrlForId(orgId)));
 	};
