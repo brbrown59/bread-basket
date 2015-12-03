135f24d9a9dd0ee69bb9f5671447af914d2dbd47
Kimberly Keller <kimberly@gravitaspublications.com>
2015-12-03 16:13:26 -0700

    added update and create to volunteer controller https://app.asana.com/0/60993780339150/70724604482007

--- public_html/js/controllers/volunteer-controller.js
+++ public_html/js/controllers/volunteer-controller.js
@@ -40,8 +40,42 @@ app.controller("VolunteerController", ["$scope", "$uibModal", "VolunteerService"
 	/**
 	 * creates a volunteer and sends it to the volunteer API
 	 *
-	 * @param volunteer
+	 * @param volunteer the volunteer to send
+	 * @param validated true if angular validated the form, false if not
 	 */
+	$scope.createVolunteer = function(volunteer, validated) {
+		if(validated === true) {
+			VolunteerService.create(volunteer)
+					.then(function(result){
+						if(result.data.staus === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message}
+						}
+					})
+		}
+	};
+
+	/**
+	 * updates a volunteer and sends it to the volunteer API
+	 *
+	 * @param volunteer the volunteer to send
+	 * @param validated true if angular validated the form, false if not
+	 */
+	$scope.updateVolunteer = function(volunteer, validated) {
+		if(validated === true && $scope.isEditing === true) {
+			VolunteerService.update(volunteer.volId, volunteer)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+
+
+		}
+	};
 
 
 	//opens the new volunteer Modal to enter new volunteer information
