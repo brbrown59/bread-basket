6fb6626352e969143719050aa21f736b96a54d21
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-12-03 09:40:10 -0700

    js Sign up Service https://app.asana.com/0/60993780339172/70723740899547

---
+++ public_html/js/services/sign-up-service.js
@@ -0,0 +1,19 @@
+/**
+ * A service for sign-up
+ *
+ * @author Tamra Fenstermaker <fenstermaker505@gmail.com>
+ * contributing code from ng-abq @author Dylan McDonald
+ */
+
+//"signupService refers to what's in the signup-controller. Todo add Sign up Service to js Signup Controller TODO is this the correct endpoint?
+app.service("SignupService", function($http){
+	this.SIGNUP_ENDPOINT = "/auth/signup";
+
+	this.signup = function(signupData) { //signupData from the signup-controller and signup-modal
+		return($http.post(this.SIGNUP_ENDPOINT, signupData)
+			.then(function(reply) {
+				return(reply.data);
+			}));
+	};
+});
+
