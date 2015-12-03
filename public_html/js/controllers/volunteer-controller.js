7723e3a4b57477cdd7c7c171b6bebcdbaf2e9a10
Kimberly Keller <kimberly@gravitaspublications.com>
2015-12-03 16:25:37 -0700

    added comma https://app.asana.com/0/60993780339150/70724604482007

--- public_html/js/controllers/volunteer-controller.js
+++ public_html/js/controllers/volunteer-controller.js
@@ -89,7 +89,7 @@ app.controller("VolunteerController", ["$scope", "$uibModal", "VolunteerService"
 		var modalHtml = '<div class="modal-body">' + message + '</div><div class="modal-footer"><button class="btn btn-primary" ng-click="yes()">Yes</button><button class="btn btn-warning" ng-click="no()">No</button></div>';
 
 		var modalInstance = $uibModal.open({
-			template: modalHtml
+			template: modalHtml,
 			controller: ModalInstanceCtrl
 		});
 
