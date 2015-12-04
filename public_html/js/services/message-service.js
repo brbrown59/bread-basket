d3fb93950191b3e24693f301dcfdcd7ef195466e
Carlos Beraun <cberaun2@cnm.edu>
2015-12-03 17:03:05 -0700

    added get blocks for services (message-service.js)---->https://app.asana.com/0/60990639151493/70724622878576

--- public_html/js/services/message-service.js
+++ public_html/js/services/message-service.js
@@ -1,2 +1,47 @@
 app.constant("MESSAGE_ENDPOINT", "api/message/");
-app.service("MessageService", function($http,MESSAGE_ENDPOINT)
+app.service("MessageService", function($http,MESSAGE_ENDPOINT) {
+			function getUrl() {
+					return(MESSAGE_ENDPOINT);
+			}
+
+	//internal function for tracking the Id on to the URL
+	function getUrlForId(messageId) {
+		return(getUrl() + messageId);
+	}
+
+	//get all function.
+	this.all = function() {
+			return($http.get(getUrl() ));
+	};
+
+	//get by Id
+	this.fetch = function(messageId){
+		return($http.get(getUrlForId(messageId)));
+	};
+
+	//get by listingId
+	this.fetchListing = function(listingId){
+		return($http.get(getUrl() + "?listing=" + listingId));
+	};
+
+	//get by orgId
+	this.fetchListing = function(orgId){
+		return($http.get(getUrl() + "?org=" + orgId));
+	};
+
+	//get by messageText
+	this.fetchListing = function(messageText){
+		return($http.get(getUrl() + "?message=" + messageText));
+	};
+
+	//post
+	this.create = function(message) {
+		return($http.post(getUrl(), message));
+	};
+
+	//put
+	this.update = function(messageIde) {
+		return($http.post(getUrl(), message));
+	};
+
+}
