83222d942a9e2b9905c9539a3615a7b8af57417f
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-12-07 14:56:24 -0700

    ListingType controller written. https://app.asana.com/0/60993780339172/70723740973486

--- public_html/js/controllers/listingtype-controller.js
+++ public_html/js/controllers/listingtype-controller.js
@@ -49,7 +49,70 @@ app.controller("ListingTypeController", ["$scope", "$uibModal", "ListingTypeServ
 		}
 	};
 
-	$scope.
+	// create new listing type (POST)
+	$scope.createListingType = function(listingType, validated) {
+		if(validated === true) {
+			ListingTypeService.create(listingType)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+		}
+	};
+
+	// update a listing type (PUT)
+	$scope.updateListingType = function(listingType, validated) {
+		if(validated === true && $scope.isEditing === true) {
+			ListingTypeService.update(listingType.listingTypeId, ListingTypeInfo)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+		}
+	};
+
+	// delete a listing type (DELETE)
+	$scope.deleteListingType = function(listingTypeId) {
+		// create a modal to ask for confirmation. should be wired to listing modal
+		var message = "Do you really want to delete this Listing Type?";
+		var modalHtml = '<div class="modal-body">' + message + '</div><div class="modal-footer"><button class="btn btn-primary" ng-click="yes()">Yes</button><button class="btn btn-warning" ng-click-"no()">No</button></div>';
+		var modalInstance = $uibModal.open({
+			template: modalHtml,
+			controller:ModalInstanceCtrl
+		});
+
+		//if yes is selcted, delete the listingType
+		modalInstance.result.then(function() {
+			ListingTypeService.destroy(listingTypeId)
+					.then(function(result) {
+						if(result.data.status === 200) {
+					$scope.alerts[0] = {type: "success", msg: result.data.message};
+				} else {
+				$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+		});
+	};
+}]);
 
 
-}])
+
+//modal instance controller for deletion prompt
+var ModalInstanceCtrl = function($scope, $uibModalInstance) {
+	$scope.yes = function() {
+		$uibModalInstance.close();
+	};
+	$scope.no = function() {
+		$uibModalInstance.dismiss('cancel');
+	};
+};
+
+
+
+
