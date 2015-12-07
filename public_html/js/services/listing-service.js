748d7ab639d95f6e11203d01f7d7ccc5375d7a5b
Carlos Beraun <cberaun2@cnm.edu>
2015-12-07 16:37:11 -0700

    added get blocks (listing-service.js)---->https://app.asana.com/0/60990639151493/70723740973486

---
+++ public_html/js/services/listing-service.js
@@ -0,0 +1,43 @@
+app.constant("LISTING_ENDPOINT", "php/api/listing/");
+app.service("ListingService", function($http,LISTING_ENDPOINT) {
+	//INTERNAL FUNCTION FOR HOLDING THE URL
+	function getUrl() {
+		return(ORGANIZATION_ENDPOINT);
+	}
+
+	//internal function for tracking the id for the URL
+	function getUrlForId(listingId) {
+			return(getUrl() + listingId);
+	}
+
+	//getting all
+	this.all = function() {
+		return($hrrp.get(getUrl()));
+	};
+
+	//get by id
+	this.fetch = function(listingId) {
+		return($http.get(getUrlForId(listingid)));
+	};
+
+	//get by organization; orgId
+	this.fetchOrganization = function(orgId) {
+		return($http.get(getUrl() + "?organization=" + orgId));
+	};
+
+	//get by Listing post Time; listingPostTime
+	this.fetch = function(listingPostTime) {
+		return($http.get(getUrl() + "?listingPostTime=" + listingPostTime));
+	};
+
+	//get by Listing Parent Id; listing
+	this.fetch = function(listingParentId) {
+		return($http.get(getUrl() + "?listingParentId=" + listingParentId));
+	};
+
+	//get by Listing Type Id;
+	this.fetch = function(listingTypeId) {
+		return($http.get(getUrl() + "?listingTypeId=" + listingTypeId));
+	};
+
+
