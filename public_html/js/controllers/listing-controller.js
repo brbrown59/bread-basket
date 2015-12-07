126ea332c6f38523edae7003cb75f0e6a5b0467a
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-12-07 16:35:08 -0700

    Listing controller. https://app.asana.com/0/60993780339172/70723740973486

--- public_html/js/controllers/listing-controller.js
+++ public_html/js/controllers/listing-controller.js
@@ -1,15 +1,53 @@
-app.controller("ListingController", ["$scope", "$uibModal", function($scope, $uibModal) {
-	$scope.listingData = {};
+app.controller("ListingController", ["$scope", "$uibModal", "ListingService", "AlertService", function($scope, $uibModal,ListingService, AlertService) {
+	$scope.ListingData = {};
+	$scope.newListing = {listingId: null, description: "", estimatedCost: "",  listingType: ""}//todo are these the correct fields?
+	$scope.isEditing = false;
+	$scope.alerts = [];
+	$scope.listings = [];
+	$scope.listingTypes = [];//Todo is linking both correct?
+
 
 	$scope.openListingModal = function () {
 		var ListingModalInstance = $uibModal.open({
 			templateUrl: "../../js/views/newlisting-modal.php",
 			controller: "ListingModal",
 			resolve: {
-				listingData : function () {
-					return($scope.listingData);
+				newListing : function () {
+					return($scope.newListing);
 				}
 			}
 		});
-	}
+		ListingModalInstance.result.then(function (newListing) {
+			$scope.newListing = newListing;
+			ListingService.create(newListing)
+					.then(function(reply) {
+						if(reply.status ===200){
+						AlertService.addAlert({type: "success", msg: reply.message});
+					} else {
+						AlertService.addAlert({type: "danger", msg: reply.message});
+					}
+			});
+	}, function() {
+		$scope.newListing = {};
+	});
+	};
+
+	/**
+	 * sets which listing is being edited and activates the editing form
+	 *
+	 * @param newListing the listing to be edited
+	 */
+
+	$scope.setEditedListing = function(newListing) {
+		$scope.isEditing = true;
+	};
+
+	/**
+	 * cancels editing doesn't change listing being edited
+	 */
+	$scope.cancelEditing = function() {
+		$scope.editedListing = {};
+		$scope.isEditing = false;
+	};
+
 }]);
