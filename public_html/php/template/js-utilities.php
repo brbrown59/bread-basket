937bf6511f497ee4e236d24dd836dad6d1dc9e0e
Kimberly Keller <kimberly@gravitaspublications.com>
2015-12-08 11:54:38 -0700

    added all the services to the js utilities file

--- public_html/php/template/js-utilities.php
+++ public_html/php/template/js-utilities.php
@@ -5,16 +5,19 @@ require_once("prefix-utilities.php");
 ?>
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/angular-password.min.js"></script>
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/breadbasket.js"></script>
+
+<!--Angular Services-->
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/services/volunteer-service.js"></script>
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/services/organization-service.js"></script>
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/services/alert-service.js"></script>
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/services/listing-service.js"></script>
-<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/tabs.js"></script>
-<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/signup-modal.js"></script>
-<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/signin-modal.js"></script>
-<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/newvolunteer-modal.js"></script>
-<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/newlisting-modal.js"></script>
-<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/contact-modal.js"></script>
+<script type="text/javascript" src="<?php echo $PREFIX; ?>js/services/listingtype-service.js"></script>
+<script type="text/javascript" src="<?php echo $PREFIX; ?>js/services/message-service.js"></script>
+<script type="text/javascript" src="<?php echo $PREFIX; ?>js/services/sign-in-service.js"></script>
+<script type="text/javascript" src="<?php echo $PREFIX; ?>js/services/sign-up-service.js"></script>
+
+<!--Angular Controllers-->
+
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/alert-controller.js"></script>
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/signup-controller.js"></script>
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/signin-controller.js"></script>
@@ -22,5 +25,15 @@ require_once("prefix-utilities.php");
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/listing-controller.js"></script>
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/contact-controller.js"></script>
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/organization-controller.js"></script>
+<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/tabs.js"></script>
+
+<!--Angular Views/Modals-->
+<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/signup-modal.js"></script>
+<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/signin-modal.js"></script>
+<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/newvolunteer-modal.js"></script>
+<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/newlisting-modal.js"></script>
+<script type="text/javascript" src="<?php echo $PREFIX; ?>js/controllers/contact-modal.js"></script>
+
+<!--Angular Directives-->
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/directives/org-view.js"></script>
 <script type="text/javascript" src="<?php echo $PREFIX; ?>js/directives/org-edit.js"></script>
