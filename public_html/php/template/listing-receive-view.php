c7c01a65c4878471768a3aa6674e947127e524c8
Carlos Beraun <cberaun2@cnm.edu>
2015-12-10 15:30:05 -0700

    Added buttons block and table block (listing-receive-view.php)---->https://app.asana.com/0/60993780339161/70724622878576

--- public_html/php/template/listing-receive-view.php
+++ public_html/php/template/listing-receive-view.php
@@ -19,10 +19,52 @@ require_once("header.php");
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h1>ALL listings Avilable</h1>
+				<h1>ALL listings Available</h1>
+			</div>
+		</div>
+	</div>
+	<hr />
+
+	<!---starts buttons-->
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-3">
+				<a class="btn btn-default btn-lg" href="login-landing-page.php" role="button"></a>
+			</div>
+			<div class="col-xs-3">
+				<button class="btn btn-info btn-lg" ng-click=""openListingModal();">New Listing available</button>
+			</div>
+		</div>
+	</div>
+	<hr />
+
+	<!-----starts table------>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<uib-alert ng-repeat="alert in alerts" type="{{ alert.type }}" close="alerts.length = 0;">{{ alert.msg}}</uib-alert>
+				<table class="table table-condensed table-striped table-hover">
+					<thead>
+						<th>Location</h3></th>
+						<th>Description</th>
+						<th>Date Posted</th>
+						<th>Actions</th>
+						<th>Details</th>
+					</thead>
+						<tr class="info" ng-repeat="listing in listings">
+							<td>Filler</td>
+							<td>{{ listing.listingMemo }}</td>
+							<td>{{ listing.listingPostTime }}</td>
+							<td>
+								<button class="btn btn-info" ng-click="openListingDetailModal();">Claim Listing</button>
+							</td>
+						</tr>
+				</table>
 			</div>
 		</div>
 	</div>
 
 </main>
-
+</div>
+</body>
+</html>
