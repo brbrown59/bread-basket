9333cf861df4701d99b3992e08ae92f361b66639
Kimberly Keller <kimberly@gravitaspublications.com>
2015-12-10 09:57:10 -0700

    finished detail modal view https://app.asana.com/0/60990639151493/70723740973486

--- public_html/js/views/listing-detailview-modal.php
+++ public_html/js/views/listing-detailview-modal.php
@@ -1,10 +1,22 @@
 <div class="modal-header">
 	<h3 class="modal-title">Details</h3>
+	<h4>Posted On: 11/11/15</h4>
 </div>
 <div class="modal-body">
-	<p>All the listing details</p>
+	<h4>Perishable</h4>
+	<h4>Description</h4>
+	<p>MEMO TEXT Offal ethical tacos, iPhone migas neutra vice yr freegan green juice williamsburg
+		bicycle rights cardigan pork belly. Retro poutine irony chartreuse craft beer you probably haven't
+		heard of them. Polaroid vice chicharrones, whatever tacos PBR&B umami single-origin coffee gluten-free
+		mixtape portland. Readymade vegan pop-up, pug scenester hammock 8-bit flexitarian master cleanse.</p>
+	<h4>Location</h4>
+	<span>123 Street NE</span> <span>Albuquerque</span> <span>NM</span> <span>87106</span>
+	<h4>Hours</h4>
+	<span>9am-5pm</span>
+	<h4>Contact Provider</h4>
+	<span>505-867-5309</span>
 </div>
 <div class="modal-footer">
-	<button class="btn btn-primary" type="button" ng-click="ok()">OK</button>
-	<button class="btn btn-warning" type="button" ng-click="cancel()">Cancel</button>
+	<button class="btn btn-primary" type="button" ng-click="ok()">Claim Listing</button>
+	<button class="btn btn-warning" type="button" ng-click="cancel()">Cancel Claim</button>
 </div>
