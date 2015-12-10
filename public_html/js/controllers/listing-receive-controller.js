1cecdbc564d86e6b9033e705ac1fccd1ca5433ee
Carlos Beraun <cberaun2@cnm.edu>
2015-12-10 16:49:11 -0700

    Added "opens edit listing modal" block (listing-receive-controller.js)---->https://app.asana.com/0/60993780339161/70724622878576

--- public_html/js/controllers/listing-receive-controller.js
+++ public_html/js/controllers/listing-receive-controller.js
@@ -1,5 +1,5 @@
 app.controller("VoulunteerController", ["$scope", "$ubiModal", "ListingService", "OrganizationService", "AlertService", function($scope, $ubiModal, ListingService, OrganizationService,AlertService) {
-	$scope.editedListing
+	$scope.editedListing = {};
 	$scope.index = null;
 	$scope.alerts = [];
 	$scope.listings = [];
@@ -28,10 +28,26 @@ app.controller("VoulunteerController", ["$scope", "$ubiModal", "ListingService",
 						$scope.alerts[0] = {type: "danger", msg: result.data.message};
 					}
 				});
-			//push the new voulunteer into the array o update live
+			//push the new listing into the array to update in real time
 			$scope.listings.push(listing);
 		});
 	};
+
+	/**
+	 * opens edit listing modal and sends updated to the volunteer API
+	 */
+
+	$scope.openEditlistingModal = function() {
+		var EditlistingModalInstance = $ubiModal.open({
+			templateUrl: "../../js/views/editListing-modal.php",
+			controller: "EditListingModal",
+			resolve: {
+				editedListing: function() {
+					return ($scope.editedListing);
+				}
+			}
+		})
+	}
 
 
 
