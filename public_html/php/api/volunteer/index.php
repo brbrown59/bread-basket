3b15032fd38af500d698ff71eb37e253b0fd2e2c
Bradley Brown <tall.white.ninja@gmail.com>
2015-12-11 10:12:39 -0700

    volunteer creation now sends emails https://app.asana.com/0/60990639151493/70724604482007

--- public_html/php/api/volunteer/index.php
+++ public_html/php/api/volunteer/index.php
@@ -3,6 +3,8 @@
 require_once dirname(dirname(__DIR__)) . "/classes/autoloader.php";
 require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once(dirname(dirname(dirname(dirname(__DIR__)))) . "/vendor/autoload.php");
+
 /**
  * controller/api for the volunteer class
  *
@@ -135,11 +137,70 @@ try {
 				$volunteer = new Volunteer($id, $_SESSION["volunteer"]->getOrgId(), $requestObject->volEmail, $emailActivation,
 					$requestObject->volFirstName, $hash, false, $requestObject->volLastName, $requestObject->volPhone, $salt);
 				$volunteer->insert($pdo);
-				//$_SESSION["volunteer"] = $volunteer;
 
 				$reply->message = "Volunteer created OK";
 
-				//compose and send the email
+				//compose and send the email for confirmation and setting a new password
+				// create Swift message
+				$swiftMessage = Swift_Message::newInstance();
+
+				// attach the sender to the message
+				// this takes the form of an associative array where the Email is the key for the real name
+				$swiftMessage->setFrom(["breadbasketapp@gmail.com" => "Bread Basket"]);
+
+				/**
+				 * attach the recipients to the message
+				 * notice this an array that can include or omit the the recipient's real name
+				 * use the recipients' real name where possible; this reduces the probability of the Email being marked as spam
+				 **/
+				$recipients = [$requestObject->volEmail];
+				$swiftMessage->setTo($recipients);
+
+				// attach the subject line to the message
+				$swiftMessage->setSubject("Please confirm your Bread Basket account");
+
+				/**
+				 * attach the actual message to the message
+				 * here, we set two versions of the message: the HTML formatted message and a special filter_var()ed
+				 * version of the message that generates a plain text version of the HTML content
+				 * notice one tactic used is to display the entire $confirmLink to plain text; this lets users
+				 * who aren't viewing HTML content in Emails still access your links
+				 **/
+
+				// building the activation link that can travel to another server and still work. This is the link that will be clicked to confirm the account.
+				$lastSlash = strrpos($_SERVER["SCRIPT_NAME"], "/");
+				$basePath = substr($_SERVER["SCRIPT_NAME"], 0, $lastSlash);
+				$lastSlash = strrpos($_SERVER["SCRIPT_NAME"], "/");
+				$basePath = substr($_SERVER["SCRIPT_NAME"], 0, $lastSlash);
+				$urlglue = $basePath . "/controllers/new-volunteer-confirmation?emailActivation=" . $volunteer->getVolEmailActivation();
+
+				$confirmLink = "https://" . $_SERVER["SERVER_NAME"] . $urlglue;
+				$message = <<< EOF
+<h1>You've been registered for the Bread Basket program!</h1>
+<p>Visit the following URL to set a new password and complete the registration process: </p>
+<a href="$confirmLink">$confirmLink</a></p>
+EOF;
+				$swiftMessage->setBody($message, "text/html");
+				$swiftMessage->addPart(html_entity_decode(filter_var($message, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)), "text/plain");
+
+				/**
+				 * send the Email via SMTP; the SMTP server here is configured to relay everything upstream via CNM
+				 * this default may or may not be available on all web hosts; consult their documentation/support for details
+				 * SwiftMailer supports many different transport methods; SMTP was chosen because it's the most compatible and has the best error handling
+				 * @see http://swiftmailer.org/docs/sending.html Sending Messages - Documentation - SwitftMailer
+				 **/
+				$smtp = Swift_SmtpTransport::newInstance("localhost", 25);
+				$mailer = Swift_Mailer::newInstance($smtp);
+				$numSent = $mailer->send($swiftMessage, $failedRecipients);
+
+				/**
+				 * the send method returns the number of recipients that accepted the Email
+				 * so, if the number attempted is not the number accepted, this is an Exception
+				 **/
+				if($numSent !== count($recipients)) {
+					// the $failedRecipients parameter passed in the send() method now contains contains an array of the Emails that failed
+					throw(new RuntimeException("unable to send email", 404));
+				}
 
 			}
 
--- public_html/php/controllers/password-confirmation-controller.php
+++
@@ -1,5 +0,0 @@
-//recieve information
-//ask them to set new password in a modal
-//handle all the password overhead, and assigning to the volunteer
-
-//FIGURE OUT HOW TO GET THE CORRECT VOLUNTEER
