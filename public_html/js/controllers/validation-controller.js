8fba827a3621059b381416c30e06fb037f8ea3f1
Bradley Brown <tall.white.ninja@gmail.com>
2015-12-10 21:16:05 -0700

    validation login page works https://app.asana.com/0/60990639151493/70724604482007

--- public_html/js/controllers/validation-controller.js
+++ public_html/js/controllers/validation-controller.js
@@ -1,3 +1,17 @@
-//this file will totally do stuff later, promise
-//should be a short one; basically sign-in without the modal; use the signin-service
-//...should I use the sign-in controller, instead?
+//uses the sign-in service; doesn't use the sign in controller because of the modal
+app.controller("ValidationController", ["$scope", "$uibModal", "$window", "AlertService", "SigninService", function($scope, $uibModal, $window, AlertService, SigninService) {
+
+	$scope.signin = function(signinData, validation) {
+		if(validation === true) {
+			SigninService.signin(signinData)
+					.then(function(reply) {
+						if(reply.status === 200) {
+							AlertService.addAlert({type: "success", msg: reply.message});
+							$window.location.assign("../../php/template/login-landing-page.php")
+						} else {
+							AlertService.addAlert({type: "danger", msg: reply.message});
+						}
+					});
+		}
+	}
+	}]);
