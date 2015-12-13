eea4558e1082b36f8ecc79d42600a4a3809ac29a
Bradley Brown <tall.white.ninja@gmail.com>
2015-12-13 15:54:40 -0700

    the if block in listing works https://app.asana.com/0/60990639151493/70723740899547

--- public_html/js/controllers/listing-controller.js
+++ public_html/js/controllers/listing-controller.js
@@ -97,6 +97,7 @@ app.controller("ListingController", ["$scope", "$uibModal", "ListingService", "A
 				.then(function(result) {
 					if(result.data.status === 200) {
 						$scope.listings = result.data.data;
+						console.log(result.data.data);
 					} else {
 						$scope.alerts[0] = {type: "danger", msg: result.data.message};
 					}
