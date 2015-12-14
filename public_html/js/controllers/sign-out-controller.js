133d3dfc8fd26d18b6af69dee9c7ea1aed92a732
Bradley Brown <tall.white.ninja@gmail.com>
2015-12-14 11:16:23 -0700

    logout redirect works https://app.asana.com/0/60990639151493/70723740899547

--- public_html/js/controllers/sign-out-controller.js
+++ public_html/js/controllers/sign-out-controller.js
@@ -1,6 +1,7 @@
-app.controller("SignoutController", ["$scope", "SignoutService", function($scope, SignoutService){
+app.controller("SignoutController", ["$scope", "SignoutService", "$window", function($scope, SignoutService, $window){
 
 	$scope.signOut = function() {
 		SignoutService.signout();
+		$window.location.assign("../../php/template/home.php")
 	}
 }]);
