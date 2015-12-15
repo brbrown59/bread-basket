48cc2ecf5b379dd6d616ec33bea475c5919ae45c
Bradley Brown <tall.white.ninja@gmail.com>
2015-12-15 08:52:55 -0700

    maps work on profile view https://app.asana.com/0/60990639151493/70723740899547

--- public_html/php/template/org-profile-view.php
+++ public_html/php/template/org-profile-view.php
@@ -57,8 +57,7 @@
 			<div class="col-md-6">
 				<div class="text-box">
 					<h3><span class="glyphicon glyphicon-pushpin"></span> Location</h3>
-					<ng-map zoom="16" center="116 Central Ave SW, Albuquerqu, NM 87102" />
-					<ng-map zoom="16" center="{organization.orgAddress1 + ' ' + organization.orgAddress2 + ' ' + organization.orgCity + ', ' + organization.orgState + ', ' + organization.orgZip}" />
+					<ng-map zoom="16" center="{{ organization.orgAddress1 }} + ' ' + {{ organization.orgAddress2 }} + ' ' + {{ organization.orgCity }} + ', ' + {{ organization.orgState }} + ', ' + {{ organization.orgZip }}" />
 				</div>
 			</div>
 		</div>
