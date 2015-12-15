9e3cd7f4d286f6fcb4badd339a908839a6146a91
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-12-15 11:30:21 -0700

    Working a contact form controller for contact swiftmailer. https://app.asana.com/0/60993780339172/70723740973486

--- public_html/js/controllers/contact-controller.js
+++ public_html/js/controllers/contact-controller.js
@@ -1,15 +1,21 @@
 app.controller("ContactController", ["$scope", "$uibModal", function($scope, $uibModal) {
 	$scope.contactData = {};
 
-	$scope.openContactModal = function () {
+	$scope.openContactModal = function() {
 		var ContactModalInstance = $uibModal.open({
 			templateUrl: "../../js/views/contact-modal.php",
 			controller: "ContactModal",
 			resolve: {
-				contactData : function () {
-					return($scope.contactData);
+				contactData: function() {
+					return ($scope.contactData);
 				}
 			}
 		});
-	}
+		ContactModalInstance.result.then(function(contactData) {
+			$scope.contactData = contactData;
+			ContactService.contact(contactData);
+
+
+		});
+	};
 }]);
---
+++ public_html/php/controllers/contact-form-controller.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * controller for the signing up a new user
+ *
+ * @author Tamra Fenstermaker <fenstermaker505@gmail.com>
+ * contributor code from https://github.com/sandidgec/foodinventory &
+ * https://github.com/Skylarity/trufork
+ **/
+
+//auto loads classes
+require_once(dirname(dirname(__DIR__)) . "/php/classes/autoloader.php");
+//security w/ NG in mind
+require_once(dirname(dirname(__DIR__)) . "/php/lib/xsrf.php");
+//a security file that's on the server created by Dylan
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+//composer for Swiftmailer
+require_once(dirname(dirname(dirname(__DIR__))) . "/vendor/autoload.php");
+
+// prepare default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->message = null;
+
+
+try {
+// verify the XSRF challenge
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
+
+// convert POSTed JSON to an object
+	$requestContent = file_get_contents("php://input");
+	$requestObject = json_decode($requestContent);
+
+
+	// create Swift message
+	$swiftMessage = Swift_Message::newInstance();
+
+
+	// attach the sender to the message
+	// this takes the form of an associative array where the Email is the key for the real name
+	$recipients = [$requestObject->email];
+	$swiftMessage->setFrom("$recipients");
+
+
+	/**
+	 * attach the recipients to the message
+	 * notice this an array that can include or omit the the recipient's real name
+	 * use the recipients' real name where possible; this reduces the probability of the Email being marked as spam
+	 **/
+
+	$swiftMessage->setTo(["breadbasketapp@gmail.com" => "Bread Basket"]);
+
+	// attach the subject line to the message
+	$swiftMessage->setSubject("Info on Bread Basket please");
+
+	/**
+	 * attach the actual message to the message
+	 * here, we set two versions of the message: the HTML formatted message and a special filter_var()ed
+	 * version of the message that generates a plain text version of the HTML content
+	 * notice one tactic used is to display the entire $confirmLink to plain text; this lets users
+	 * who aren't viewing HTML content in Emails still access your links
+	 **/
+
+
+
+	$message = [$requestObject->message];
+	$swiftMessage->setBody($message, "text/html");
+	$swiftMessage->addPart(html_entity_decode(filter_var($message, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)), "text/plain");
+
+	/**
+	 * send the Email via SMTP; the SMTP server here is configured to relay everything upstream via CNM
+	 * this default may or may not be available on all web hosts; consult their documentation/support for details
+	 * SwiftMailer supports many different transport methods; SMTP was chosen because it's the most compatible and has the best error handling
+	 * @see http://swiftmailer.org/docs/sending.html Sending Messages - Documentation - SwitftMailer
+	 **/
+	$smtp = Swift_SmtpTransport::newInstance("localhost", 25);
+	$mailer = Swift_Mailer::newInstance($smtp);
+	$numSent = $mailer->send($swiftMessage, $failedRecipients);
+
+	/**
+	 * the send method returns the number of recipients that accepted the Email
+	 * so, if the number attempted is not the number accepted, this is an Exception
+	 **/
+	if($numSent !== count($recipients)) {
+		// the $failedRecipients parameter passed in the send() method now contains contains an array of the Emails that failed
+		throw(new RuntimeException("unable to send email"));
+	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+	$reply->message = $exception->getTrace();
+}
+
+header("Content-type: application/json");
+echo json_encode($reply);
