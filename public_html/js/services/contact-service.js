5f71151d7a7921ada41d8f3ab1f239db991ee02d
Carlos Beraun <cberaun2@cnm.edu>
2015-12-15 11:36:45 -0700

    Added service for contact(contact-service.js)---->https://app.asana.com/0/60993780339161/70724622878576

---
+++ public_html/js/services/contact-service.js
@@ -0,0 +1,19 @@
+/**
+ * A service for sign-in
+ *
+ * @author Tamra Fenstermaker <fenstermaker505@gmail.com>
+ * contributing code from ng-abq @author Dylan McDonald
+ */
+
+//"signinService refers to what's in the signin-controller. Todo add Sign in Service to js SignIn Controller TODO is this the correct endpoint?
+app.service("ContactService", function($http){
+	this.CONTACT_ENDPOINT = "../../php/controllers/contact-form-controller.php";
+
+	this.contact = function(contactData) { //contactData from the contact-form-controller and contact-modal
+		return($http.post(this.CONTACT_ENDPOINT, contactData)
+			.then(function(reply) {
+				return(reply.data);
+			}));
+	};
+});
+
